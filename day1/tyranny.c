5870682890d4f6ace0b3a602bcf60e24289ec0ac
Charles Comstock <dgtized@gmail.com>
2019-12-01 19:58:45 -0600

    Use rfuel for residuals calculation

--- day1/tyranny.c
+++ day1/tyranny.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
+int fuel(int mass) {
+     return mass / 3 - 2;
+}
+
+int rfuel(int mass) {
+     int f = fuel(mass);;
+     int t = f;
+     while((f = fuel(f)) > 0) {
+          t += f;
+     }
+     return t;
+}
+
 int main(int argc, char **argv) {
      const char *filename = argv[1];
      FILE *file = fopen(filename, "r");
      int num = 0;
-     int out = 0;
 
      int result = 0;
      while(fscanf(file, "%d\n", &num) != EOF) {
-          out = num / 3 - 2;
-          printf("%d -> %d\n", num, out);
-          result += out;
+          printf("%d -> %d\n", num, rfuel(num));
+          result += rfuel(num);;
      }
      printf("Sum: %d\n", result);
 
