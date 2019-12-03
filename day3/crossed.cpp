b287c26f56147657aceeeb8f69e6866d4eefa01e
Charles Comstock <dgtized@gmail.com>
2019-12-03 00:10:15 -0600

    functioning

--- day3/crossed.cpp
+++ day3/crossed.cpp
@@ -1,7 +1,42 @@
 #include <fstream>
+#include <sstream>
 #include <cassert>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
 
 // g++ crossed.cpp && ./a.out input
+
+std::vector<std::pair<int, int>> trace(const std::string& s) {
+  std::vector<std::pair<int, int>> path;
+
+  std::string token;
+
+  int x = 0;
+  int y = 0;
+
+  std::istringstream seq(s);
+  while(std::getline(seq, token, ',')) {
+    char direction;
+    int distance;
+    sscanf(token.c_str(), "%c%d", &direction, &distance);
+    for(int i = 0; i < distance; i++) {
+      if(direction == 'U') {
+        y++;
+      } else if(direction == 'D') {
+        y--;
+      } else if(direction == 'L') {
+        x--;
+      } else if(direction == 'R') {
+        x++;
+      }
+      //printf("%d %d\n", x, y);
+      path.push_back(std::make_pair(x, y));
+    }
+  }
+
+  return path;
+}
 
 int main(int argc, char **argv) {
   assert(argc == 2);
@@ -15,12 +50,21 @@ int main(int argc, char **argv) {
 
   printf("%s\n%s\n", lineA.c_str(), lineB.c_str());
 
-  std::pair<int,int> a(0,0);
-  std::pair<int,int> b(0,1);
-  std::pair<int,int> c(0,0);
+  auto pathA = trace(lineA);
+  auto pathB = trace(lineB);
+  int min_manhatten = 0;
 
-  assert(a == c);
-  assert(a != b);
+  for(auto it = pathA.begin(); it < pathA.end(); it++) {
+    if(std::find(pathB.begin(), pathB.end(), *it) != pathB.end()) {
+      int manhattan = abs(it->first) + abs(it->second);
+      if(manhattan < min_manhatten || min_manhatten == 0) {
+        min_manhatten = manhattan;
+      }
+      printf("match %d %d\n", it->first, it->second);
+    }
+  }
+
+  printf("minimum distance %d\n", min_manhatten);
 
   return 0;
 }
