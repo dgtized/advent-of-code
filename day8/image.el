e057eaf4982a8d9c33e1b838d567f201bebef1bd
Charles Comstock <dgtized@gmail.com>
2019-12-08 03:02:51 -0600

    Improve documentation and refactor slightly

--- day8/image.el
+++ day8/image.el
@@ -1,3 +1,4 @@
+(require 's)
 (require 'f)
 
 (defvar image-width 25)
@@ -12,29 +13,35 @@
 ;; (s-count-matches "0" "00120") => 3
 
 (defun image-checksum ()
-  (let ((layers (image->layers "input")))
-    (car (cl-sort
-          (cl-loop for layer in layers
-                   collect (list (s-count-matches "0" layer)
-                                 (* (s-count-matches "1" layer)
-                                    (s-count-matches "2" layer)))
-                   )
-          '< :key 'car))))
+  "Find the layer with least number of zeroes and count product of count of 1's and 2's. "
+  (let ((counts (cl-loop for layer in (image->layers "input")
+                         collect (list (s-count-matches "0" layer)
+                                       (* (s-count-matches "1" layer)
+                                          (s-count-matches "2" layer))))))
+    (car (cl-sort counts '< :key 'car))))
 
 ;; (image-checksum)
 
 (defun transposed-pixels ()
+  "Transpose each layer into a list of pixels, where each pixel
+is a list of the pixel values across every layers."
   (cl-loop for i below layer-width
            collect (cl-loop for layer in (image->layers "input")
                             collect (substring layer i (1+ i)))))
 
+(defun first-visible (pixels)
+  "Drops all leading transparent pixels and returns first pixel after."
+  (car (seq-drop-while (lambda (pixel) (equalp "2" pixel)) pixels)))
+
 (defun visible-layer ()
+  "Build a layer of visible pixels from the transposed list"
   (cl-loop for pixels in (transposed-pixels)
-           concat (car (seq-drop-while (lambda (pixel) (equalp "2" pixel)) pixels))))
+           concat (first-visible pixels)))
 
-
-(with-current-buffer (pop-to-buffer "*image output*")
-  (erase-buffer)
-  (let ((visible (visible-layer)))
+(defun generate-image (layer)
+  "Render an image from a particular layer, split into individual rows."
+  (with-temp-file "image.sif"
     (cl-loop for i below layer-width by image-width
-             do (insert (substring visible i (+ i image-width)) "\n"))))
+             do (insert (substring layer i (+ i image-width)) "\n"))))
+
+;; (generate-image (visible-layer))
