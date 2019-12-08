454c69b288977a8dd106b83c3b7fcd215c17eeb6
Charles Comstock <dgtized@gmail.com>
2019-12-08 02:04:13 -0600

    wrap in function

--- day8/image.el
+++ day8/image.el
@@ -9,14 +9,17 @@
 
 ;; (s-count-matches "0" "00120") => 3
 
-(let ((layers (image->layers "input")))
-  (car (cl-sort
-        (cl-loop for layer in layers
-                 collect (list (s-count-matches "0" layer)
-                               (* (s-count-matches "1" layer)
-                                  (s-count-matches "2" layer)))
-                 )
-        '< :key 'car)))
+(defun image-checksum ()
+  (let ((layers (image->layers "input")))
+    (car (cl-sort
+          (cl-loop for layer in layers
+                   collect (list (s-count-matches "0" layer)
+                                 (* (s-count-matches "1" layer)
+                                    (s-count-matches "2" layer)))
+                   )
+          '< :key 'car))))
+
+;; (image-checksum)
 
 
 
