2b79f67184dfa76a8e8d3b49915699ce33289ada
Charles Comstock <dgtized@gmail.com>
2019-12-09 02:12:54 -0600

    add test cases with validation

--- day9/relative-intcode.rkt
+++ day9/relative-intcode.rkt
@@ -184,7 +184,14 @@
     (run-until-blocked (cpu memory 0 0 0 1 'run) ports)
     ports))
 
+(define (port-output ports)
+  (vector-ref ports 1))
+
 ;; quine
-(run-source "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99")
-(run-source "1102,34915192,34915192,7,4,7,99,0")
-(run-source "104,1125899906842624,99")
+(let* ((program "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99")
+       (out (port-output (run-source program))))
+  (list out (equal? program (string-join (map number->string out) ","))))
+(let ((out (car (port-output (run-source "1102,34915192,34915192,7,4,7,99,0")))))
+  (list out (= 16 (string-length (number->string out)))))
+(let ((out (car (port-output (run-source "104,1125899906842624,99")))))
+  (list out (= 1125899906842624 out)))
