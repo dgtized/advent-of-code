54698f728fe19dbbac1cd7b56a77999b9a42b5bc
Charles Comstock <dgtized@gmail.com>
2019-12-10 00:25:14 -0600

    parse and load points

--- day10/monitor_station.scala
+++ day10/monitor_station.scala
@@ -1,9 +1,24 @@
 import scala.io.Source
 
+case class Point(x: Int, y: Int)
+
 object MonitorStation extends App {
   val lines = Source.fromFile(args(0)).getLines.toList
+  var points = List[Point]()
 
+  var y : Int = 0
   lines.foreach { line =>
     println(line)
+
+    for(x <- 0 to line.length-1) {
+      if(line(x) == '#') {
+        points = Point(x, y) :: points
+      }
+    }
+    y = y + 1
+  }
+
+  points.foreach { point =>
+    println(point)
   }
 }
