191473be5d97467e5323455a36296cb5d6b9708e
Charles Comstock <dgtized@gmail.com>
2019-12-14 14:58:33 -0600

    remove comment

--- day14/lib/stochiometry.ex
+++ day14/lib/stochiometry.ex
@@ -1,17 +1,4 @@
 defmodule Stochiometry do
-  @moduledoc """
-  Documentation for Stochiometry.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Stochiometry.hello()
-      :world
-
-  """
   def hello do
     :world
   end
