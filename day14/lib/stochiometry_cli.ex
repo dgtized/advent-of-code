f1417deb817dccaee3d9cac605079e753cf52823
Charles Comstock <dgtized@gmail.com>
2019-12-14 14:05:46 -0600

    Keep track of partial result

--- day14/lib/stochiometry_cli.ex
+++ day14/lib/stochiometry_cli.ex
@@ -4,6 +4,8 @@ defmodule Stochiometry.CLI do
   def main(args) do
     conversions = load_converions(args)
     IO.inspect(conversions)
+
+    IO.inspect(calc(conversions, [{"FUEL", 1}], []))
   end
 
   def load_converions(args) do
@@ -17,13 +19,56 @@ defmodule Stochiometry.CLI do
         String.split(ingredients, ",")
         |> Enum.map(&split_part/1)
 
-      [split_part(result), deps]
+      {n, c} = split_part(result)
+      %{n => {c, deps}}
     end)
+    |> Enum.reduce(&Map.merge/2)
+  end
+
+  def calc(conversions, [{source, requested} | remainder], residuals) do
+    IO.inspect({[source, requested], residuals})
+
+    if source == "ORE" do
+      requested + calc(conversions, remainder, residuals)
+    else
+      {count, deps} = conversions[source]
+
+      batches = Float.ceil(requested / count)
+
+      next =
+        Enum.map(deps, fn {dep, required} ->
+          {dep, batches * required}
+        end)
+
+      calc(conversions, combine(remainder ++ next), residuals)
+    end
+  end
+
+  def calc(conversions, [], residuals) do
+    0
+  end
+
+  def combine(lst) do
+    IO.inspect(["combine", lst])
+
+    r =
+      Enum.group_by(lst, fn {n, c} ->
+        n
+      end)
+      |> Map.values()
+      |> Enum.map(fn similar ->
+        {name, _} = List.first(similar)
+        {name, Enum.reduce(similar, 0, fn {_, v}, acc -> acc + v end)}
+      end)
+
+    IO.inspect(["combine", r])
+
+    r
   end
 
   def split_part(part) do
     [count | [name | _]] = String.trim(part) |> String.split(" ")
     {count, _} = Integer.parse(count)
-    [name, count]
+    {name, count}
   end
 end
