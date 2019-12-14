888b57531b67c91a21e2e472dbbd244d054e640c
Charles Comstock <dgtized@gmail.com>
2019-12-14 03:35:02 -0600

    Basic parsing structure

--- day14/lib/stochiometry_cli.ex
+++ day14/lib/stochiometry_cli.ex
@@ -1,5 +1,29 @@
+# mix escript.build && ./stochiometry input
+
 defmodule Stochiometry.CLI do
-  def main(_args) do
-    IO.puts("hi")
+  def main(args) do
+    conversions = load_converions(args)
+    IO.inspect(conversions)
+  end
+
+  def load_converions(args) do
+    File.read!(args)
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      [ingredients | [result | _]] = String.trim(line) |> String.split(" => ")
+
+      deps =
+        String.split(ingredients, ",")
+        |> Enum.map(&split_part/1)
+
+      [split_part(result), deps]
+    end)
+  end
+
+  def split_part(part) do
+    [count | [name | _]] = String.trim(part) |> String.split(" ")
+    {count, _} = Integer.parse(count)
+    [name, count]
   end
 end
