da3b0f75cd1bd42de1af2b712ae3e4ef5d21f5fb
Charles Comstock <dgtized@gmail.com>
2019-12-15 18:53:53 -0600

    basic world printing and navigation

--- day15/oxygen-intcode.rkt
+++ day15/oxygen-intcode.rkt
@@ -7,7 +7,8 @@
     [(n) 1]
     [(s) 2]
     [(w) 3]
-    [(e) 4]))
+    [(e) 4]
+    [else null]))
 
 (struct robot (x y) #:transparent)
 (struct tile (x y type) #:transparent)
@@ -36,17 +37,58 @@
          (code (car (vector-ref ports 1))))
     (cons cpu (result bot dir code))))
 
+(define (world-coords world)
+  (let ((xs (map tile-x world))
+        (ys (map tile-y world)))
+    (let ((xmin (apply min xs))
+          (xmax (apply max xs))
+          (ymin (apply min ys))
+          (ymax (apply max ys)))
+      ;; (list (+ 1 (abs xmin) (abs xmax)) (abs xmin)
+      ;;       (+ 1 (abs ymin) (abs ymax)) (abs ymin))
+
+      (list 40 10 50 40))))
+
+(define (update-tile grid x y type)
+  (vector-set! grid y
+               (let ((copy (string-copy (vector-ref grid y))))
+                 (string-set! copy x (case type
+                                       [(wall) #\#]
+                                       [(empty) #\.]
+                                       [(oxygen) #\G]
+                                       [(robot) #\D]))
+                 copy)))
+
+(define (print-world bot world)
+  (match (world-coords world)
+    [(list xsize xoffset ysize yoffset)
+     (let ((grid (build-vector ysize (lambda (x) (make-string xsize #\ )))))
+       (for [(piece world)]
+         (match piece
+           [(tile x y type)
+            (update-tile grid
+                         (+ x xoffset)
+                         (+ y yoffset)
+                         type)]))
+       (match bot
+         [(robot rx ry)
+          (update-tile grid (+ rx xoffset) (+ ry yoffset) 'robot)])
+       (for ([row (vector->list grid)])
+         (println row)))]))
+
 (define (repl cpu bot world)
-  (println (list bot world))
+  (print-world bot world)
   (println "Input: ")
   (let ((input (read)))
-    (println (list input (key->dir input)))
+    (println (list input (key->dir input) bot))
     (if (eqv? input 'q)
         (list bot world)
-        (match (travel cpu bot (key->dir input))
-          [(list ncpu nbot tile)
-           (repl ncpu nbot (cons tile world))]))))
+        (if (null? (key->dir input))
+            (repl cpu bot world) ;; no change
+            (match (travel cpu bot (key->dir input))
+              [(list ncpu nbot tile)
+               (repl ncpu nbot (cons tile world))])))))
 
 (let* ((program (load-program "input"))
        (bot (robot 0 0)))
-  (repl (init-cpu program 0 1) bot '()))
+  (repl (init-cpu program 0 1) bot (list (tile 0 0 'empty))))
