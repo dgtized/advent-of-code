c8481decaead5aa49aca8c6e68122554eff6827d
Charles Comstock <dgtized@gmail.com>
2019-12-19 00:53:31 -0600

    Use match-lambda to unpack results

--- day19/tractor.rkt
+++ day19/tractor.rkt
@@ -1,13 +1,15 @@
 #lang racket
 
+(require racket/match)
 (require "../lib/intcode.rkt")
 
 (let* ((program (init-cpu (load-program "input") 0 1)))
   (foldl + 0
          (map
-          (lambda (pair)
-            (let ((ports (vector (list (car pair) (cadr pair)) '())))
-              (run-until-blocked program ports)
-              (car (vector-ref ports 1))))
+          (match-lambda
+            [(list x y)
+             (let ((ports (vector (list x y) '())))
+               (run-until-blocked program ports)
+               (car (vector-ref ports 1)))])
           (cartesian-product (range 0 50)
                              (range 0 50)))))
