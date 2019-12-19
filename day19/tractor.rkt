cecc4c5ba1eb93a7686722cae0008dd5e9021c15
Charles Comstock <dgtized@gmail.com>
2019-12-19 01:10:04 -0600

    Simplify

--- day19/tractor.rkt
+++ day19/tractor.rkt
@@ -1,15 +1,15 @@
 #lang racket
 
-(require racket/match)
 (require "../lib/intcode.rkt")
 
-(let* ((program (init-cpu (load-program "input") 0 1)))
+(let ((program (init-cpu (load-program "input") 0 1)))
+  (define (pulling? args)
+    (let ((ports (vector args '())))
+      (run-until-blocked program ports)
+      (car (vector-ref ports 1))))
   (foldl + 0
-         (map
-          (match-lambda
-            [(list x y)
-             (let ((ports (vector (list x y) '())))
-               (run-until-blocked program ports)
-               (car (vector-ref ports 1)))])
-          (cartesian-product (range 0 50)
-                             (range 0 50)))))
+         (map pulling?
+              (cartesian-product (range 0 50)
+                                 (range 0 50)))))
+
+
