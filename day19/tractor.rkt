3f384caf2cd8018530e113645ab0aced51b16c21
Charles Comstock <dgtized@gmail.com>
2019-12-20 01:48:21 -0600

    narrow down the candidates and solve day19.2

--- day19/tractor.rkt
+++ day19/tractor.rkt
@@ -25,27 +25,32 @@
                                 (range 0 2000)))))
       (list (first scan) (last scan))))
 
-  ;; candidates
-  ;; (println (map (lambda (x) (list x (h-scan x) (v-scan x))) (range 200 1000 100)))
-  '((200 (228 268) (150 175))
-    (300 (342 402) (224 263))
-    (400 (456 536) (299 351))
-    (500 (570 670) (373 438))
-    (600 (684 804) (448 526))
-    (700 (798 938) (522 614))
-    (800 (912 1073) (597 702))
-    (900 (1026 1207) (671 790)))
 
   (define (pulling? x y)
     (let ((res (tractor-pull (list x y))))
-      (println (list x y res))
       (= 1 res)))
 
   (define (satisfies? x y)
     (and (pulling? x y)
-         (pulling? (+ x 100) y)
-         (pulling? x (+ y 100))
-         (pulling? (+ x 100) (+ y 100))))
+         (pulling? (+ x 99) y)
+         (pulling? x (+ y 99))
+         (pulling? (+ x 99) (+ y 99))))
 
-  (println (satisfies? 80 1000)))
+  ;; candidates
+  (for [(y (range 1050 1070 1))]
+    (match-let* ([(list xmin xmax) (h-scan y)])
+      (let ((cx (- xmax 100)))
+        (match-let ([(list ymin ymax) (v-scan cx)])
+          (let ((cy (- ymax 100)))
+            (when (and (> cx xmin)
+                       (> cy ymin)
+                       (satisfies? cx cy))
+              (println (list y cx cy (+ (* 10000 cx) cy)
+                             (list 'x xmin xmax) (list 'y ymin ymax)
+                             ))))))))
+
+  (for*/first [(x (range 1250 1400 1))
+               (y (range 1000 1100 1))
+               #:when (satisfies? x y)]
+    (list x y (+ (* 10000 x) y))))
 
