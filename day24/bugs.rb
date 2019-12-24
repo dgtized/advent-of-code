a583fdc5c6c3f898d9c25fd60c7c851800c921b3
Charles Comstock <dgtized@gmail.com>
2019-12-24 00:56:24 -0600

    initial sketch for day24

---
+++ day24/bugs.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'matrix'
+
+def read_grid(filename)
+  remap = { '.' => 0, '#' => 1 }
+  initial = IO.readlines(filename).map(&:chomp).map do |row|
+    row.scan(/./).map { |x| remap[x] }
+  end
+  Matrix[*initial]
+end
+
+def surroundings(i, j)
+  [
+    [i - 1, j], [i, j - 1], [i, j + 1], [i + 1, j]
+  ].select { |x, y| (0...5).include?(x) && (0...5).include?(y) }
+end
+
+def cycle(grid)
+  next_grid = grid.to_a
+  5.times do |i|
+    5.times do |j|
+      cells = surroundings(i,j)
+      alive = grid.element(i,j) > 0
+      adjacent = cells.count { |x,y| grid.element(x,y) > 0 }
+      next_grid[i][j] =
+        case
+        when alive && adjacent == 1
+          1
+        when !alive && [1,2].include?(adjacent)
+          1
+        else
+          0
+        end
+    end
+  end
+  Matrix[*next_grid]
+end
+
+grid = read_grid('input.test')
+p grid
+p cycle(grid)
+p cycle(cycle(grid))
