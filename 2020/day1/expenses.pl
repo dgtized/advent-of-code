3ebd39ed4a0b1473d4531aae6495e01d7675e70b
Charles Comstock <dgtized@gmail.com>
2020-12-01 16:47:36 -0600

    Add documentation to day1 example

--- 2020/day1/expenses.pl
+++ 2020/day1/expenses.pl
@@ -1,14 +1,19 @@
 #!/usr/bin/env perl
 
+# usage: perl expenses.pl input
+
 use strict;
 use warnings;
 use 5.010;
 
 chomp(my @expenses = <>);
 
+# Separate the expenses into likely pairs of elements <1000 and >=1000 to reduce
+# search space
 my @lessthan = grep("$_" < 1000, @expenses);
 my @greaterthan = grep("$_" >= 1000, @expenses);
 
+# Find expense pairs that add up to 2020 and report the product
 for my $less (@lessthan) {
   for my $greater (@greaterthan) {
     if (($less + $greater) == 2020) {
@@ -17,6 +22,8 @@ for my $less (@lessthan) {
   }
 }
 
+# Find expense triplets that add up to 2020 and report the product
+# Note: this reports the solution twice, as it loops over the smaller set twice.
 for my $less (@lessthan) {
   for my $less2 (@lessthan) {
     next if $less == $less2;
