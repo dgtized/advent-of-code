60921ef5371a09227fc00c25aa342e97a9404580
Charles Comstock <dgtized@gmail.com>
2020-12-03 23:56:29 -0600

    Fix rubocop complaints

--- 2020/day4/passports.rb
+++ 2020/day4/passports.rb
@@ -2,50 +2,51 @@
 
 passports = IO.read("input").split("\n\n").map do |raw|
   passport = {}
-  raw.scan(/([a-z]{3}):(\S+)/).each { |m|
+  raw.scan(/([a-z]{3}):(\S+)/).each do |m|
     passport[m[0]] = m[1]
-  }
+  end
   passport
 end
 
 printf "Passports: %d\n", passports.size
 
-expected = %w{byr iyr eyr hgt hcl ecl pid cid}
+expected = %w[byr iyr eyr hgt hcl ecl pid cid]
 
 valid_by_fields = passports.select do |passport|
   diff = expected - passport.keys
-  diff.empty? || diff == ["cid"]
+  diff.empty? || diff == ['cid']
 end
 
 printf "Valid Passports: %d\n", valid_by_fields.size
 
 fully_valid = valid_by_fields.select do |passport|
   conditions = []
-  byr = passport["byr"].match(/(\d{4})/)[1].to_i
+  byr = passport['byr'].match(/(\d{4})/)[1].to_i
   conditions << (byr >= 1920 && byr <= 2002)
 
-  iyr = passport["iyr"].match(/(\d{4})/)[1].to_i
+  iyr = passport['iyr'].match(/(\d{4})/)[1].to_i
   conditions << (iyr >= 2010 && iyr <= 2020)
 
-  eyr = passport["eyr"].match(/(\d{4})/)[1].to_i
+  eyr = passport['eyr'].match(/(\d{4})/)[1].to_i
   conditions << (eyr >= 2020 && eyr <= 2030)
 
   conditions << (
-    if m = passport["hgt"].match(/(\d+)(cm|in)/)
+    m = passport['hgt'].match(/(\d+)(cm|in)/)
+    if m
       hgt = m[1].to_i
-      if m[2] == "cm"
+      if m[2] == 'cm'
         hgt >= 150 && hgt <= 193
-      elsif m[2] == "in"
+      elsif m[2] == 'in'
         hgt >= 59 && hgt <= 76
       end
     end
   )
 
-  conditions << passport["hcl"].match(/#[0-9a-f]{6}$/)
+  conditions << passport['hcl'].match(/#[0-9a-f]{6}$/)
 
-  conditions << %w{amb blu brn gry grn hzl oth}.include?(passport["ecl"])
+  conditions << %w[amb blu brn gry grn hzl oth].include?(passport['ecl'])
 
-  conditions << passport["pid"].match(/^\d{9}$/)
+  conditions << passport['pid'].match(/^\d{9}$/)
 
   conditions.all?
 end
