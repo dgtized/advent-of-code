30a0dff65e535c72caace32d86a59e9ed29a0078
Charles Comstock <dgtized@gmail.com>
2020-12-06 00:10:59 -0600

    Day6 part2 solution using bash *join*

--- 2020/day6/custom-customs.sh
+++ 2020/day6/custom-customs.sh
@@ -7,3 +7,15 @@ for g in $groups; do
 done
 
 echo "Answers From Anyone:" $sum
+
+for g in $(awk NF=NF RS="" FS="\n" OFS=":" $1); do
+    echo $g | cut -d: -f1 | fold -1 | sort > group_answers
+    for i in $(echo $g | sed -e 's/:/\n/g'); do
+        echo $i | fold -1 | sort | join group_answers - > join_group
+        mv join_group group_answers
+    done
+    v=$(cat group_answers | tr -d "\n" | wc -c)
+    ((everyone += v))
+done
+
+echo "Answers From Everyone:" $everyone
