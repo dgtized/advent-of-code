77ed2a9e290c3d248e5bc7f413ae1c62094b87d5
Charles Comstock <dgtized@gmail.com>
2020-12-06 00:20:54 -0600

    rename to input and add usage statement

--- 2020/day6/custom-customs.sh
+++ 2020/day6/custom-customs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
+# usage ./custom-customs.sh input
+input=$1
+
 # separate each group into a single line and iterate over them
-groups=$(cat $1 | tr "\n" " " | sed -e 's/  /\n/g' | sed -e 's/ //g')
+groups=$(cat $input | tr "\n" " " | sed -e 's/  /\n/g' | sed -e 's/ //g')
 for g in $groups; do
     # take all the votes in the group as line, change them to a column with
     # fold, sort with unique and then flatten again with tr so as to count the
@@ -13,7 +16,7 @@ done
 echo "Answers From Anyone:" $sum
 
 # separate each group into a line, with each individuals votes separated by :
-for g in $(awk NF=NF RS="" FS="\n" OFS=":" $1); do
+for g in $(awk NF=NF RS="" FS="\n" OFS=":" $input); do
     # grab the first individuals votes and create a list of answers
     echo $g | cut -d: -f1 | fold -1 | sort > group_answers
 
