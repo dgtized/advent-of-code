edfde21414125c4a6da7da974bf73c696ff6b3fa
Charles Comstock <dgtized@gmail.com>
2020-12-09 00:17:09 -0600

    Second star for day9 in scala
    I really feel like there is a nicer functional way to scan this with takeWhile
    and a running sum, but struggled with Scala

--- 2020/day9/encoding_error.scala
+++ 2020/day9/encoding_error.scala
@@ -3,7 +3,7 @@
 import scala.io.Source
 
 object H {
-  def sum(preceding: List[Long], goal: Long) : Boolean = {
+  def checksum(preceding: List[Long], goal: Long) : Boolean = {
     for(base <- 0 to preceding.length - 2) {
       val a = preceding(base)
       for(ahead <- base + 1 to preceding.length - 1) {
@@ -14,6 +14,21 @@ object H {
     }
     return false
   }
+
+  def contiguous(numbers: List[Long], goal: Long, offset: Int) : Unit = {
+    for(n <- 0 to offset - 2 ) {
+      val base = numbers.drop(n)
+      var total : Long = 0
+      for(u <- n + 1 to offset - 1) {
+        val chunk = base.take(u - n)
+        val sum = chunk.sum
+        if(sum == goal) {
+          println(chunk)
+          println("Weakness: " + (chunk.min + chunk.max))
+        }
+      }
+    }
+  }
 }
 
 object EncodingError extends App {
@@ -23,8 +38,10 @@ object EncodingError extends App {
   for(i <- preamble to numbers.length - 1) {
     val discarded = i - preamble;
     val preceding = numbers.drop(discarded).take(preamble)
-    if(!H.sum(preceding, numbers(i))) {
-      println("First Star: " + numbers(i))
+    if(!H.checksum(preceding, numbers(i))) {
+      println("Checksum Error: " + numbers(i))
+
+      H.contiguous(numbers, numbers(i), i)
     }
   }
 }
