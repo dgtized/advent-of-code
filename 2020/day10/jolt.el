6acffcb0002f0d53585230310c49ac97a577fa4a
Charles Comstock <dgtized@gmail.com>
2020-12-10 00:47:23 -0600

    Add comments to explain

--- 2020/day10/jolt.el
+++ 2020/day10/jolt.el
@@ -3,21 +3,29 @@
 (require 'dash)
 
 (defun read-adapters (filename)
+  "Read all the lines from `filename' into a list, convert to
+numbers and sort ascending."
   (sort (mapcar #'string-to-number
                 (s-lines (s-trim (f-read-text filename))))
         #'<))
 
 (defun jolt-differences (adapters)
+  "Find the jolt difference between each `adapter'.
+
+Note that 0 is the joltage of source, and built-in adapter is 3
+more than last adapter used."
   (let ((built-in (+ (car (last adapters)) 3)))
     (cl-loop for a in (cons 0 adapters)
              for b in (-snoc adapters built-in)
              collect (- b a))))
 
-(defun count-frequencies (amt differences)
-  (cl-loop for d in differences
-           count (= d amt)))
+(defun count-frequencies (value differences)
+  "Number of times `value' occurs in `differences'."
+  (cl-loop for x in differences
+           count (= x value)))
 
 (defun first-star (filename)
+  "Count 1s and 3s from differences in adapters, and multiply"
   (let* ((adapters (read-adapters filename))
          (differences (jolt-differences adapters))
          (ones (count-frequencies 1 differences))
@@ -32,6 +40,7 @@
 ;; (first-star "input") ;; => (71 31 2201)
 
 (defun cases (n)
+  "Permutation count for a sequence of 1s of length `n'"
   (cl-case n
     (1 1)
     (2 2)
@@ -39,10 +48,15 @@
     (4 7)))
 
 (defun second-star (filename)
+  "Calculate number of permutations of viable adapter joltage sequences"
   (let* ((adapters (read-adapters filename))
+         ;; Remove any differences of size 3 as it's impossible to remove those gaps
          (sets (-split-on 3 (jolt-differences adapters)))
+         ;; Each group is a set of consecutive 1s, so we lookup for a group of
+         ;; that size how many permutations are possible.
          (permutations (cl-loop for group in sets
                                 collect (cases (length group)))))
+    ;; Calculate the product of each group with permutations
     (-reduce '* permutations)))
 
 ;; (second-star "example.1") ;; 8
