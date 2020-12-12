a18a92f28dd82f72c54183b1b327b19e9280516c
Charles Comstock <dgtized@gmail.com>
2020-12-12 00:20:46 -0600

    Solve second star for day12
    Calculate the rotation for the waypoint using a rotation matrix, and switch back
    to float64 but round each result to avoid errors from propagating.

--- 2020/day12/rain_risk.go
+++ 2020/day12/rain_risk.go
@@ -56,7 +56,55 @@ func firstStar(lines []string) {
 	fmt.Println("Star 1 Distance: ", int(math.Abs(float64(pos.x)) + math.Abs(float64(pos.y))))
 }
 
+type Vector struct {
+	x float64
+	y float64
+}
+
+func rotate(degrees int, waypoint Vector) Vector {
+	theta := float64(degrees) * math.Pi / 180
+	x := float64(waypoint.x)
+	y := float64(waypoint.y)
+	return Vector{
+		math.Round(x * math.Cos(theta) - y * math.Sin(theta)),
+		math.Round(x * math.Sin(theta) + y * math.Cos(theta)),
+	};
+}
+
+func secondStar(lines []string) {
+	pos := Vector{0,0}
+	waypoint := Vector{10,1}
+
+	for i, line := range lines {
+		if line == "" {break}
+		dir := line[0:1]
+		amount, _ := strconv.Atoi(line[1:])
+		value := float64(amount)
+		switch dir {
+		case "N":
+			waypoint.y += value
+		case "E":
+			waypoint.x += value
+		case "S":
+			waypoint.y -= value
+		case "W":
+			waypoint.x -= value
+		case "L":
+			waypoint = rotate(+amount, waypoint)
+		case "R":
+			waypoint = rotate(-amount, waypoint)
+		case "F":
+			pos.x += math.Round(value * waypoint.x)
+			pos.y += math.Round(value * waypoint.y)
+		}
+		fmt.Println(i, dir, value, pos, waypoint)
+	}
+
+	fmt.Println("Star 2 Distance: ", int(math.Abs(pos.x) + math.Abs(pos.y)))
+}
+
 func main() {
 	lines := parseFile(os.Args[1])
 	firstStar(lines)
+	secondStar(lines)
 }
