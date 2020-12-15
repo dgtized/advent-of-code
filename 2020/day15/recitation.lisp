6676f140866a9079e80e34db8f90a7e1c3eda7ff
Charles Comstock <dgtized@gmail.com>
2020-12-15 16:23:07 -0600

    Solve star2 using hash table lookup

--- 2020/day15/recitation.lisp
+++ 2020/day15/recitation.lisp
@@ -1,12 +1,5 @@
-(defun solve-next (lst)
-  (let* ((previous (position (car lst) (cdr lst))))
-    (if previous
-        (cons (1+ previous) lst)
-        (cons 0 lst))))
-
 (defun solve-next-lookup (lst iter seen)
   (let ((previous (gethash (car lst) seen)))
-    (princ (list iter 'lookup (car lst) previous)) (terpri)
     (if previous
         (- iter previous)
         0)))
@@ -16,21 +9,31 @@
       lst
       (let* ((next (solve-next-lookup lst iter seen)))
         (setf (gethash (car lst) seen) iter)
-        (princ (list iter 'set (car lst) iter)) (terpri)
         (solve-up-to (cons next lst) seen (1+ iter) n))))
 
 (defun solve-to-n (lst n)
   (let ((seen (make-hash-table)))
+    (loop for i from 1 to (length lst)
+          for elem in (reverse (cdr lst))
+          do (setf (gethash elem seen) i))
     (solve-up-to lst
                  seen
                  (length lst)
                  n)))
 
-(princ "Test example: ")
-(princ (solve-to-n (reverse '(0 3 6)) 10)) (terpri)
-(princ (reverse '(0 3 6 0 3 3 1 0 4 0))) (terpri)
-;; (time (car (solve-up-to (reverse '(0 3 6)) 3 300000)))
-;; (princ "First star: ")
-;; (princ (car (solve-to-n (reverse '(0 5 4 1 10 14 7)) 2020)))
-;; (terpri)
-;; (time (princ (car (solve-up-to (reverse '(0 5 4 1 10 14 7)) 7 300000))))
+(princ "Test example @ 2020: ")
+(princ (car (solve-to-n (reverse '(0 3 6)) 2020))) (terpri)
+
+(princ "First star: ")
+(princ (car (solve-to-n (reverse '(0 5 4 1 10 14 7)) 2020)))
+(terpri)
+
+(defun run-test ()
+  (princ "Test Example @ 30000000: ")
+  (time (princ (= 175594 (car (solve-to-n (reverse '(0 3 6)) 30000000)))))
+  (terpri)
+  (princ "Second Star:")
+  (time (princ (= 9007186 (car (solve-to-n (reverse '(0 5 4 1 10 14 7)) 30000000)))))
+  (terpri))
+
+
