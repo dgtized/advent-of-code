edf438e0b2331f2f0759327186113bdd8a8a2b61
Charles Comstock <dgtized@gmail.com>
2020-12-15 01:26:35 -0600

    Save partial solution that isn't working

--- 2020/day15/recitation.lisp
+++ 2020/day15/recitation.lisp
@@ -1,34 +1,36 @@
-(declaim (ftype (function (list) list)))
 (defun solve-next (lst)
-  (declare (optimize (speed 3) (safety 0)))
   (let* ((previous (position (car lst) (cdr lst))))
     (if previous
         (cons (1+ previous) lst)
         (cons 0 lst))))
 
-;; (solve-next (reverse '(0 3 6))) ;; 0
-;; (solve-next (reverse '(0 3 6 0))) ;; 3
-;; (solve-next (reverse '(0 3 6 0 3))) ;; 3
-;; (solve-next (reverse '(0 3 6 0 3 3))) ;; 1
-;; (solve-next (reverse '(0 3 6 0 3 3 1))) ;; 0
-;; (solve-next (reverse '(0 3 6 0 3 3 1 0))) ;; 4
-;; (solve-next (reverse '(0 3 6 0 3 3 1 0 4))) ;; 0
+(defun solve-next-lookup (lst iter seen)
+  (let ((previous (gethash (car lst) seen)))
+    (princ (list iter 'lookup (car lst) previous)) (terpri)
+    (if previous
+        (- iter previous)
+        0)))
+
+(defun solve-up-to (lst seen iter n)
+  (if (= iter n)
+      lst
+      (let* ((next (solve-next-lookup lst iter seen)))
+        (setf (gethash (car lst) seen) iter)
+        (princ (list iter 'set (car lst) iter)) (terpri)
+        (solve-up-to (cons next lst) seen (1+ iter) n))))
 
 (defun solve-to-n (lst n)
-  (if (= n (length lst))
-      lst
-      (solve-next (solve-to-n lst (1- n)))))
+  (let ((seen (make-hash-table)))
+    (solve-up-to lst
+                 seen
+                 (length lst)
+                 n)))
 
-;; Tried making tail recusive and optimizing
-(declaim (ftype (function (list fixnum fixnum) list) solve-up-to))
-(defun solve-up-to (lst c n)
-  (declare (optimize (speed 3) (safety 0)))
-  (if (= c n)
-      lst
-      (solve-up-to (solve-next lst) (1+ c) n)))
-
-(car (solve-to-n (reverse '(0 3 6)) 2020))
+(princ "Test example: ")
+(princ (solve-to-n (reverse '(0 3 6)) 10)) (terpri)
+(princ (reverse '(0 3 6 0 3 3 1 0 4 0))) (terpri)
 ;; (time (car (solve-up-to (reverse '(0 3 6)) 3 300000)))
-(princ "First star: ")
-(princ (car (solve-up-to (reverse '(0 5 4 1 10 14 7)) 7 2020)))
+;; (princ "First star: ")
+;; (princ (car (solve-to-n (reverse '(0 5 4 1 10 14 7)) 2020)))
+;; (terpri)
 ;; (time (princ (car (solve-up-to (reverse '(0 5 4 1 10 14 7)) 7 300000))))
