58e612a85f3176c7fc774cb2548a9b95456e7528
Charles Comstock <dgtized@gmail.com>
2020-12-15 00:07:44 -0600

    Second star is trickier

--- 2020/day15/recitation.lisp
+++ 2020/day15/recitation.lisp
@@ -1,6 +1,7 @@
+(declaim (ftype (function (list) list)))
 (defun solve-next (lst)
-  (let* ((last (car lst))
-         (previous (position last (cdr lst))))
+  (declare (optimize (speed 3) (safety 0)))
+  (let* ((previous (position (car lst) (cdr lst))))
     (if previous
         (cons (1+ previous) lst)
         (cons 0 lst))))
@@ -18,6 +19,16 @@
       lst
       (solve-next (solve-to-n lst (1- n)))))
 
+;; Tried making tail recusive and optimizing
+(declaim (ftype (function (list fixnum fixnum) list) solve-up-to))
+(defun solve-up-to (lst c n)
+  (declare (optimize (speed 3) (safety 0)))
+  (if (= c n)
+      lst
+      (solve-up-to (solve-next lst) (1+ c) n)))
+
 (car (solve-to-n (reverse '(0 3 6)) 2020))
+;; (time (car (solve-up-to (reverse '(0 3 6)) 3 300000)))
 (princ "First star: ")
-(princ (car (solve-to-n (reverse '(0 5 4 1 10 14 7)) 2020)))
+(princ (car (solve-up-to (reverse '(0 5 4 1 10 14 7)) 7 2020)))
+;; (time (princ (car (solve-up-to (reverse '(0 5 4 1 10 14 7)) 7 300000))))
