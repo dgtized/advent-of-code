2506903ed2c7efbad47f2813e744ec219663d090
Charles Comstock <dgtized@gmail.com>
2020-12-19 16:31:22 -0600

    Add changed rules & example2, not working though

--- 2020/day19/monster_messages.clj
+++ 2020/day19/monster_messages.clj
@@ -2,14 +2,39 @@
   (:require [instaparse.core :as insta]
             [clojure.string :as str]))
 
-(defn count-valid [file]
+(defn fix [rules change-rules]
+  (if (empty? change-rules)
+    rules
+    (let [change (first change-rules)
+          prefix (second (re-find #"^(\d+:) " change))]
+      (recur (map #(str/replace % (re-pattern (str prefix ".*$")) change) rules)
+             (rest change-rules)))))
+
+(comment (fix ["0: 1" "1: 2" "2: 3"]
+              ["0: 2" "1: 2"])
+         (fix ["0: 1"] []))
+
+(defn count-valid [file change-rules]
   (let [[rules messages] (map str/split-lines (str/split (slurp file) #"\n\n"))
-        grammar (into ["S = 0"] (map #(str/replace % ": " " = ") rules))
+        fixed-rules (fix rules change-rules)
+        grammar (into ["S = 0"] (map #(str/replace % ": " " = ") fixed-rules))
+        _ (do (newline)
+              (println (str "Rules: " (count grammar)))
+              (println (str/join "\n" (map #(str "\"" % "\"") grammar))))
         parser (insta/parser (str/join "\n" grammar))
         valid (remove (fn [msg] (insta/failure? (insta/parse parser msg))) messages)]
     (count valid)))
 
 (comment
-  (count-valid "example")
-  (count-valid "input"))
+  (= 2 (count-valid "example" []))
+  (= 3 (count-valid "example2" []))
+  (= 147 (count-valid "input" []))
+
+  (= (count-valid "example2" ["8: 42 | 42 8"
+                              "11: 42 31 | 42 11 31"])
+     12)
+
+  (count-valid "input" ["8: 42 | 42 8"
+                        "11: 42 31 | 42 11 31"])
+  )
 
