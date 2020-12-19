20ddef7f2aebd6ba2ae094b4cef442bdbfe04514
Charles Comstock <dgtized@gmail.com>
2020-12-19 03:30:53 -0600

    Refactored to clarify algorithm to see any concerns better

--- 2020/day19/monster_messages.rb
+++ 2020/day19/monster_messages.rb
@@ -1,5 +1,40 @@
 #!/usr/bin/env ruby
 
+class Path
+  def self.from(input)
+    if(input.kind_of?(Array) && input.all? { |x| x.kind_of?(String)})
+      Path.new(input.join)
+    else
+      Path.new(input)
+    end
+  end
+
+  attr_reader :seq
+  def initialize(seq)
+    @seq = seq
+  end
+
+  def terminal?
+    @seq.is_a?(String)
+  end
+
+  def can_substitute?(id)
+    !terminal? && @seq.include?(id)
+  end
+
+  def substitute(id, value)
+    if(!terminal?)
+      Path.from(seq.flat_map { |x| x == id ? value : x })
+    else
+      self
+    end
+  end
+
+  def inspect
+    "#Path:%p" % [seq]
+  end
+end
+
 class Rule
   attr_reader :id, :choices
   def initialize(id, choices)
@@ -7,50 +42,29 @@ class Rule
   end
 
   def terminal?(n = 1)
-    choices.all? { |x| x.kind_of?(String) } && choices.size <= n
-  end
-
-  def collapse
-    if terminal?
-      self
-    elsif choices.kind_of?(Array)
-      Rule.new(
-        id,
-        choices.map do |path|
-          if(path.kind_of?(Array) && path.all? { |x| x.kind_of?(String) })
-            path.join
-          else
-            path
-          end
-        end
-      )
-    end
+    choices.all?(&:terminal?) && choices.size <= n
   end
 
   def substitute(rule)
-    if choices.kind_of?(Array)
-      alts = []
-      choices.each do |path|
-        if(path.kind_of?(Array) && path.include?(rule.id))
-          Array(rule.choices).each do |rpath|
-            alts << path.map { |x| x == rule.id ? rpath : x }
-          end
-        else
-          alts << path
+    alts = choices.flat_map do |path|
+      if(path.can_substitute?(rule.id))
+        Array(rule.choices).map do |rpath|
+          path.substitute(rule.id, rpath.seq)
         end
+      else
+        [path]
       end
-      Rule.new(id, alts)
-    else
-      self
     end
+
+    Rule.new(id, alts)
   end
 end
 
 def simplify(rules)
-  while terminal = rules.find { |r| r.terminal?(1) }
+  while terminal = rules.find { |r| r.terminal?(10_000000) }
     break if rules.size == 1
     rules.delete(terminal)
-    rules = rules.map { |r| r.substitute(terminal).collapse }
+    rules = rules.map { |r| r.substitute(terminal) }
   end
 
   rules
@@ -60,7 +74,7 @@ def matching?(rules, msg)
   #index = rules.each_with_object({}) { |acc,x| acc[x.id] = x }
 
   r = rules.find { |r| r.id == 0 }
-  r.choices.include?(msg)
+  r.choices.map(&:seq).include?(msg)
 end
 
 def parse(file)
@@ -69,9 +83,11 @@ def parse(file)
   rules = lines.take_while { |line| line.match(/\d+:/) }.map do |line|
     m = line.match(/(\d+): (.*)$/)
     choices = if terminal = m[2].match(/"(.)"/)
-      [terminal[1]]
+      [Path.new(terminal[1])]
     else
-      m[2].split("|").map { |r| r.chomp.split(" ").map(&:to_i)}
+      m[2].split("|").map do |r|
+        Path.from(r.chomp.split(" ").map(&:to_i))
+      end
     end
 
     Rule.new(m[1].to_i, choices)
