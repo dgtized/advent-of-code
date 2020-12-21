ef25fe4cfa683edfa31ec85951a0fb5e3c5f46a3
Charles Comstock <dgtized@gmail.com>
2020-12-21 01:41:13 -0600

    Solve 21.2

--- 2020/day21/allergen_assessment.clj
+++ 2020/day21/allergen_assessment.clj
@@ -29,10 +29,32 @@
                        (apply set/union))]
     (count (remove probables (mapcat second input)))))
 
+(defn assign-allergens [probables]
+  (reduce (fn [proven [allergen ingredients]]
+            (assoc proven allergen
+                   (first (apply disj ingredients
+                                 (flatten (vals proven))))))
+          {}
+          (sort-by (comp count second) probables)))
+
+(defn second-star [filename]
+  (->> filename
+       parse
+       probable-ingredients
+       assign-allergens
+       (sort-by first)
+       (map second)
+       (str/join ",")))
+
 (comment
-  (probable-ingredients (parse "example"))
-  (probable-ingredients (parse "input"))
+  (assign-allergens (probable-ingredients (parse "example")))
+  (assign-allergens (probable-ingredients (parse "input")))
 
   (= 5 (first-star "example"))
   (= 2786 (first-star "input"))
+
+  (= (second-star "example")
+     "mxmxvkd,sqjhc,fvjkl")
+  (= (second-star "input")
+     "prxmdlz,ncjv,knprxg,lxjtns,vzzz,clg,cxfz,qdfpq")
   )
