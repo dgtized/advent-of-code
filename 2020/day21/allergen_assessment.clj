5dca32516d5204833b9e6cc5f96599c580159c2e
Charles Comstock <dgtized@gmail.com>
2020-12-21 01:51:09 -0600

    Add comments to day21

--- 2020/day21/allergen_assessment.clj
+++ 2020/day21/allergen_assessment.clj
@@ -10,7 +10,12 @@
     [(str/split allergens #", ")
      (str/split ingredients #" ")]))
 
-(defn probable-ingredients [input]
+(defn winnow-ingredients
+  "Reduces [[allergens ...] => [ingredients ...]] to {allergen #{ingredients ...} ...}
+
+  For each allergen, iteratively winnows the list of ingredients by intersecting
+  with each subsequent reference to the allergen."
+  [input]
   (reduce (fn [known [allergens ingredients]]
             (reduce (fn [prove allergen]
                       (update prove allergen
@@ -21,34 +26,43 @@
           {}
           input))
 
-(defn first-star [filename]
+(defn first-star
+  "Removes the union of all likely ingredients to count ingredients remaining. "
+  [filename]
   (let [input (parse filename)
         probables (->> input
-                       probable-ingredients
+                       winnow-ingredients
                        vals
                        (apply set/union))]
     (count (remove probables (mapcat second input)))))
 
-(defn assign-allergens [probables]
+(defn assign-allergens
+  "Uniquely assign allergen -> ingredient
+
+  Sorts probable ingredients so smallest set is first, assumes that is a single
+  ingredient, assigns it as proven to be that allergen and then reduces each
+  subsequent mapping by removing any proven ingredients."
+  [probables]
   (reduce (fn [proven [allergen ingredients]]
             (assoc proven allergen
                    (first (apply disj ingredients
-                                 (flatten (vals proven))))))
+                                 (vals proven)))))
           {}
           (sort-by (comp count second) probables)))
 
-(defn second-star [filename]
+(defn second-star
+  [filename]
   (->> filename
        parse
-       probable-ingredients
+       winnow-ingredients
        assign-allergens
        (sort-by first)
        (map second)
        (str/join ",")))
 
 (comment
-  (assign-allergens (probable-ingredients (parse "example")))
-  (assign-allergens (probable-ingredients (parse "input")))
+  (assign-allergens (winnow-ingredients (parse "example")))
+  (assign-allergens (winnow-ingredients (parse "input")))
 
   (= 5 (first-star "example"))
   (= 2786 (first-star "input"))
