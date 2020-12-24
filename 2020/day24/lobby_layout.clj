c9e20e840ed440aa3bef1bffe6186afc3b544232
Charles Comstock <dgtized@gmail.com>
2020-12-24 00:22:04 -0600

    Solve day24.2

--- 2020/day24/lobby_layout.clj
+++ 2020/day24/lobby_layout.clj
@@ -40,6 +40,54 @@
       frequencies
       (get false)))
 
+(defn neighboring-coordinates [position]
+  (map (comp (partial vector-add position) follow)
+       ["e" "se" "sw" "w" "nw" "ne"]))
+
+(defn assoc-if-missing [map key value]
+  (if (contains? map key)
+    map
+    (assoc map key value)))
+
+(defn add-missing-neighbors [grid]
+  (->> (keys grid)
+       (mapcat neighboring-coordinates)
+       distinct
+       (reduce (fn [grid coord] (assoc-if-missing grid coord true))
+               grid)))
+
+(defn count-neighbors [grid coord]
+  (get (frequencies (map (fn [p] (get grid p true))
+                         (neighboring-coordinates coord)))
+       false 0))
+
+(defn grid-step [grid]
+  ;; (println (first-star grid))
+  (into {}
+        (for [[coord value] (add-missing-neighbors grid)
+              :let [black (count-neighbors grid coord)]]
+          (cond (and (= value false) (or (= black 0) (> black 2)))
+                [coord true]
+                (and (= value true) (= black 2))
+                [coord false]
+                :else
+                [coord value]))))
+
+(defn second-star [grid n]
+  (first-star (last (take (+ 1 n) (iterate grid-step grid)))))
+
 (comment
   (first-star (calculate-grid "example"))
-  (first-star (calculate-grid "input")))
+  (first-star (calculate-grid "input"))
+
+  (neighboring-coordinates [0 0])
+  (add-missing-neighbors {[0 0] false [0 1] false})
+
+  (= 1 (count-neighbors {[0 0] false [1 0] false} [0 0]))
+
+  (add-missing-neighbors (calculate-grid "example"))
+  (grid-step (calculate-grid "example"))
+
+  (= 2208 (second-star (calculate-grid "example") 100))
+  (= 3737 (second-star (calculate-grid "input") 100))
+  )
