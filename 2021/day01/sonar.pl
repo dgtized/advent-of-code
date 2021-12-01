2c7277feaaff6e41b9657a60de1c098f51eb0bf0
Charles Comstock <dgtized@gmail.com>
2021-11-30 23:35:58 -0600

    cleanup script so both first and second star work

--- 2021/day01/sonar.pl
+++ 2021/day01/sonar.pl
@@ -9,25 +9,22 @@ use v5.10;
 my @readings = <>;
 chomp(@readings);
 
-# my $count = 0;
-
-# my $last = shift @readings;
-
-# for my $v (@readings) {
-#   if ($v > $last) {
-#     $count++;
-#   }
-#   $last = $v;
-# }
-
-# say $count;
+my $count = 0;
+my $last = $readings[0];
+for my $v (@readings[1..scalar(@readings)-1]) {
+  if ($v > $last) {
+    $count++;
+  }
+  $last = $v;
+}
+say "First Star: ", $count;
 
 my $count2 = 0;
 for(my $i = 0; $i < scalar(@readings); $i++) {
   if ($i > 2) {
-    my $d = $readings[$i - 3];
-    my $c = $readings[$i - 2];
-    my $b = $readings[$i - 1];
+    my $d = $readings[$i-3];
+    my $c = $readings[$i-2];
+    my $b = $readings[$i-1];
     my $a = $readings[$i];
     my $s1 = $d + $c + $b;
     my $s2 = $c + $b + $a;
@@ -37,4 +34,4 @@ for(my $i = 0; $i < scalar(@readings); $i++) {
   }
 }
 
-say $count2;
+say "Second Star: ", $count2;
