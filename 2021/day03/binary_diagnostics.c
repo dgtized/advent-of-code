085529122526360c5d797200336a727fcb2f9fb5
Charles Comstock <dgtized@gmail.com>
2021-12-04 08:56:29 -0600

    apply gnu styling instead of k&r

--- 2021/day03/binary_diagnostics.c
+++ 2021/day03/binary_diagnostics.c
@@ -4,16 +4,16 @@
 // gcc binary_diagnostics.c -o binary_diagnostics && ./binary_diagnostics input
 
 int bitcheck(char lines[1000][60], int length, int pos) {
-     int on = 0;
-     int off = 0;
-     for(int i = 0; i < length; i++) {
-          if(lines[i][pos] == '0')
-               off++;
-          if(lines[i][pos] == '1')
-               on++;
-     }
-     printf("%d %d\n", on, off);
-     return (on > off);
+  int on = 0;
+  int off = 0;
+  for(int i = 0; i < length; i++) {
+    if(lines[i][pos] == '0')
+      off++;
+    if(lines[i][pos] == '1')
+      on++;
+  }
+  printf("%d %d\n", on, off);
+  return (on > off);
 }
 
 int main(int argc, char **argv) {
@@ -32,12 +32,12 @@ int main(int argc, char **argv) {
   int gamma = 0;
   int epsilon = 0;
   for(int j = 0; j < width; j++) {
-       int bitvalue = 1 << (width-j-1);
-       if(bitcheck(lines, length, j)) {
-            gamma += bitvalue;
-       } else {
-            epsilon += bitvalue;
-       }
+    int bitvalue = 1 << (width-j-1);
+    if(bitcheck(lines, length, j)) {
+      gamma += bitvalue;
+    } else {
+      epsilon += bitvalue;
+    }
   }
 
   printf("First Star: %d %d -> %d\n", gamma, epsilon, gamma * epsilon);
