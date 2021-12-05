dfe525fe2faacca7227f4aa28a2bb43d18863dbe
Charles Comstock <dgtized@gmail.com>
2021-12-05 00:46:07 -0600

    factor ink-line out of line and diag-line

--- 2021/day05/hydrothermal_venture.clj
+++ 2021/day05/hydrothermal_venture.clj
@@ -12,22 +12,27 @@
                     rest
                     (mapv edn/read-string))))))
 
-(defn line [screen [x0 y0 x1 y1]]
+(defn ink-line [screen points]
+  (reduce (fn [m p] (update m p (fnil inc 0)))
+          screen
+          points))
+
+(defn line [[x0 y0 x1 y1]]
   (cond (= x0 x1)
-        (reduce (fn [m y] (update m [x0 y] (fnil inc 0)))
-                screen
-                (range (min y0 y1) (inc (max y0 y1))))
+        (mapv vector
+              (repeatedly (constantly x0))
+              (range (min y0 y1) (inc (max y0 y1))))
         (= y0 y1)
-        (reduce (fn [m x] (update m [x y0] (fnil inc 0)))
-                screen
-                (range (min x0 x1) (inc (max x0 x1))))
-        :else
-        screen))
+        (mapv vector
+              (range (min x0 x1) (inc (max x0 x1)))
+              (repeatedly (constantly y0)))
+        :else []))
 
 (defn part1 [filename]
   (->> filename
        parse-lines
-       (reduce line {})
+       (mapv line)
+       (reduce ink-line {})
        vals
        (remove #(= 1 %))
        count))
@@ -35,39 +40,35 @@
 (assert (= 5 (part1 "example")))
 (assert (= 7380 (part1 "input")))
 
-(defn diag-line [screen [x0 y0 x1 y1]]
+(defn diag-line [[x0 y0 x1 y1]]
   (let [x-range (range (min x0 x1) (inc (max x0 x1)))
         y-range (range (min y0 y1) (inc (max y0 y1)))]
-    (reduce (fn [m p] (update m p (fnil inc 0)))
-            screen
-            (cond (= x0 x1)
-                  (mapv vector
-                        (repeatedly (constantly x0))
-                        y-range)
-                  (= y0 y1)
-                  (mapv vector
-                        x-range
-                        (repeatedly (constantly y0)))
-                  :else
-                  (mapv vector
-                        (if (> x0 x1) (reverse x-range) x-range)
-                        (if (> y0 y1) (reverse y-range) y-range))))))
+    (cond (= x0 x1)
+          (mapv vector
+                (repeatedly (constantly x0))
+                y-range)
+          (= y0 y1)
+          (mapv vector
+                x-range
+                (repeatedly (constantly y0)))
+          :else
+          (mapv vector
+                (if (> x0 x1) (reverse x-range) x-range)
+                (if (> y0 y1) (reverse y-range) y-range)))))
 
-(comment (diag-line {} [0 9 5 9])
-         (diag-line {} [8 0 0 8])
-         (diag-line {} [1 1 3 3])
-         (diag-line {} [9 7 7 9]))
+(comment (diag-line [0 9 5 9])
+         (diag-line [8 0 0 8])
+         (diag-line [1 1 3 3])
+         (diag-line [9 7 7 9]))
 
 (defn part2 [filename]
   (->> filename
        parse-lines
-       (reduce diag-line {})
+       (mapv diag-line)
+       (reduce ink-line {})
        vals
        (remove #(= 1 %))
        count))
 
 (assert (= 12 (part2 "example")))
 (assert (= 21373 (part2 "input")))
-
-
-
