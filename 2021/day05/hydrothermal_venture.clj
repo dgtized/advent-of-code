03940dc75bb8034db9bb77f6967d5fd4d078400b
Charles Comstock <dgtized@gmail.com>
2021-12-05 00:10:08 -0600

    partially working part2

--- 2021/day05/hydrothermal_venture.clj
+++ 2021/day05/hydrothermal_venture.clj
@@ -31,7 +31,40 @@
 (assert (= 5 (part1 "example")))
 (assert (= 7380 (part1 "input")))
 
+(defn line2 [screen [x0 y0 x1 y1]]
+  (let [x-range (range (min x0 x1) (inc (max x0 x1)))
+        y-range (range (min y0 y1) (inc (max y0 y1)))]
+    (reduce (fn [m p] (update m p (fnil inc 0)))
+            screen
+            (cond (= x0 x1)
+                  (mapv vector
+                        (repeatedly (constantly x0))
+                        y-range)
+                  (= y0 y1)
+                  (mapv vector
+                        (range (min x0 x1) (inc (max x0 x1)))
+                        x-range)
+                  :else
+                  (mapv vector
+                        (if (> x0 x1) (reverse x-range) x-range)
+                        (if (> y0 y1) (reverse y-range) y-range))))))
 
+(comment (line2 {} [8 0 0 8])
+         (line2 {} [1 1 3 3])
+         (line2 {} [9 7 7 9]))
+
+(defn part2 [filename]
+  (->> filename
+       parse-lines
+       (reduce line2 {})
+       ;; vals
+       ;; (remove #(= 1 %))
+       ;; count
+
+       ))
+
+;; (assert (= 12 (part2 "example")))
+;; (assert (= 0 (part2 "input")))
 
 
 
