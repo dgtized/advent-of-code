6f1c025ee6547eae7a9b61e78171fb7ae85db069
Charles Comstock <dgtized@gmail.com>
2021-12-06 23:36:13 -0600

    solution to part1 day07 in cpp

--- 2021/day07/whale-treachery.cpp
+++ 2021/day07/whale-treachery.cpp
@@ -9,6 +9,33 @@
 
 using namespace std;
 
+int cost(vector<int> &positions, int pos) {
+  int cost = 0;
+  for(auto v : positions) {
+    cost += abs(pos-v);
+  }
+  return cost;
+}
+
+int min_cost(vector<int> positions) {
+  sort(positions.begin(), positions.end());
+
+  int a = positions.front();
+  int b = positions.back();
+  cout << "min_cost: " << a << " " << b << endl;
+  int best_cost = 10000000;
+  int best_idx = -1;
+  for(int i = a; i <= b; i++) {
+    int c = cost(positions, i);
+    if(c < best_cost) {
+      best_cost = c;
+      best_idx = i;
+    }
+  };
+
+  return best_idx;
+}
+
 int main(int argc, char **argv) {
   ifstream file(argv[1]);
 
@@ -18,7 +45,15 @@ int main(int argc, char **argv) {
     positions.push_back(atoi(str.c_str()));
   }
 
+  float average = 0.0;
   for(auto v : positions) {
-    cout << v << endl;
+    average += v;
   }
+  average = (average / positions.size());
+
+  cout << average << endl;
+  cout << cost(positions, 2) << endl;
+
+  int min = min_cost(positions);
+  cout << "First Star: @" << min << " -> " << cost(positions, min) << endl;
 }
