6d6f3541882b66fb3199d373d08a0d23043f9541
Charles Comstock <dgtized@gmail.com>
2021-12-07 23:51:13 -0600

    add some notes on canonical representations and sort the order of input

--- 2021/day08/seven_segment_search.clj
+++ 2021/day08/seven_segment_search.clj
@@ -1,21 +1,51 @@
 (ns seven-segment-search
   (:require [clojure.string :as str]))
 
+(defn parse-line [line]
+  (map (fn [s] (map sort (str/split s #"\s+"))) (str/split line #"\|\s+")))
+
 (defn parse [filename]
-  (for [line (str/split-lines (slurp filename))]
-    (map (fn [s] (str/split s #"\s+")) (str/split line #"\|\s+"))))
+  (mapv parse-line (str/split-lines (slurp filename))))
+
+;; Canonical representation:
+;;  aaaa
+;; b    c
+;; b    c
+;;  dddd
+;; e    f
+;; e    f
+;;  gggg
+
+(def known-digits
+  {2 1 ;; cf
+   3 7 ;; acf
+   4 4 ;; bcdf
+   7 8 ;; abcdefg
+   })
+
+;; 9 [abcdfg] missing e
+;; 6 [abdefg] missing c
+;; 5 [abdfg]  missing c,e
+;; 3 [acdfg]  missing b,e
+;; 2 [acdeg]  missing b,f
+;; 0 [abcefg] missing d
 
 (defn part1 [input]
-  (let [digits {2 1
-                3 7
-                4 4
-                7 8}
-        freqs (->> input
+  (let [freqs (->> input
                    (map second)
-                   (mapcat #(map (comp digits count) %))
+                   (mapcat #(map (comp known-digits count) %))
                    frequencies)]
     (apply + (vals (dissoc freqs nil)))))
 
 (assert (= 26 (part1 (parse "example"))))
 (assert (= 352 (part1 (parse "input"))))
 
+(def ex2 (parse-line "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"))
+
+(defn solve-digits [digits]
+  (let [base (zipmap digits
+                     (for [digit digits]
+                       (known-digits (count digit))))]
+    base))
+
+(comment (solve-digits (first ex2)))
