bf6998feb38206d88f3cbabb16d87af9644b7111
Charles Comstock <dgtized@gmail.com>
2021-12-07 23:18:44 -0600

    day08 first star in clojure

---
+++ 2021/day08/seven_segment_search.clj
@@ -0,0 +1,22 @@
+(ns seven-segment-search
+  (:require [clojure.string :as str]))
+
+(defn parse [filename]
+  (for [line (str/split-lines (slurp filename))]
+    (map (fn [s] (str/split s #"\s+")) (str/split line #"\|\s+"))))
+
+(def digits {2 1
+             3 7
+             4 4
+             7 8})
+
+(defn part1 [input]
+  (let [freqs (->> input
+                   (map second)
+                   (mapcat #(map (comp digits count) %))
+                   frequencies)]
+    (apply + (vals (dissoc freqs nil)))))
+
+(assert (= 26 (part1 (parse "example"))))
+(assert (= 352 (part1 (parse "input"))))
+
