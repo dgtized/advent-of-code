8e4324222e7b7a779b00f479e9d06eedc16696a0
Charles Comstock <dgtized@gmail.com>
2021-12-09 23:16:44 -0600

    extract walk from check and move points into part1

--- 2021/day10/syntax_scoring.clj
+++ 2021/day10/syntax_scoring.clj
@@ -15,22 +15,24 @@
 (def open (set (keys pairs)))
 (def close (set (vals pairs)))
 
-(def points {")" 3 "]" 57 "}" 1197 ">" 25137})
+(defn walk [syms]
+  (reduce (fn [stack sym]
+            (cond (open sym)
+                  (conj stack sym)
+                  (close sym)
+                  (if (= sym (get pairs (peek stack)))
+                    (pop stack)
+                    (reduced sym))))
+          [] syms))
 
 (defn check [syms]
-  (let [s (reduce (fn [stack sym]
-                    (cond (open sym)
-                          (conj stack sym)
-                          (close sym)
-                          (if (= sym (get pairs (peek stack)))
-                            (pop stack)
-                            (reduced sym))))
-                  [] syms)]
+  (let [s (walk syms)]
     (when (string? s)
       s)))
 
 (defn part1 [input]
-  (apply + (map points (keep check input))))
+  (let [points {")" 3 "]" 57 "}" 1197 ">" 25137}]
+    (apply + (map points (keep check input)))))
 
 (assert (= 26397 (part1 (parse "example"))))
 (assert (= 268845 (part1 (parse "input"))))
