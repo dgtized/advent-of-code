2c50687dfdb875278faaaab48596992fe28368a8
Charles Comstock <dgtized@gmail.com>
2021-12-11 00:50:07 -0600

    basic parsing for clojure 1.11.0-alpha3

---
+++ 2021/day11/dumbo_octopus.clj
@@ -0,0 +1,26 @@
+(ns dumbo-octopus
+  (:require [clojure.string :as str]))
+
+(defn parse [filename]
+  (mapv (fn [line] (mapv (comp parse-long str) (seq line)))
+        (str/split-lines (slurp filename))))
+
+(defn grid->map [grid]
+  (into {}
+        (for [i (range 10)
+              j (range 10)]
+          {[i j] (nth (nth grid j) i)})))
+
+(defn map->grid [map]
+  (->> (for [i (range 10)
+             j (range 10)]
+         (get map [i j]))
+       (partition 10)
+       (mapv vec)))
+
+;; (defn step [grid]
+;;   (let [m (grid->map grid)]))
+
+(comment (map->grid (grid->map (parse "example"))))
+
+
