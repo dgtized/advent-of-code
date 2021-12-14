6e60365d8728e0e4c1b9f7f245a073802a673950
Charles Comstock <dgtized@gmail.com>
2021-12-14 00:28:50 -0600

    various experiments

--- 2021/day14/polymer.clj
+++ 2021/day14/polymer.clj
@@ -30,5 +30,46 @@
 (assert (= 1588 (second (part1 10 (parse "example")))))
 
 (assert (= 2703 (second (part1 10 (parse "input")))))
+
+(defn part2 [step input]
+  (take (inc step) (map (comp frequencies :template) (iterate apply-rules input))))
+
+;; (part2 16 (parse "example"))
+(take-last 2 (part2 12 (parse "input")))
+
+;; (for [i (range 1 20)]
+;;   (time (second (part1 i (parse "example")))))
+
+(defn app-rule [rules template]
+  (apply str (concat (map str
+                          template
+                          (for [[a b] (partition 2 1 template)]
+                            (rules (str a b))))
+                     [(last template)])))
+
+(defn pair-expansion [step pair rules]
+  (take (inc step) (iterate (partial app-rule rules) pair)))
+
+(let [{:keys [rules]} (parse "example")]
+  (for [form ["NN" "NC" "CB"]]
+    (pair-expansion 6 form rules)))
+
+(let [{:keys [rules]} (parse "example")]
+  (for [form ["NN" "NC" "CB"]]
+    (map (comp (partial sort-by first) frequencies) (pair-expansion 6 form rules))))
+
+(defn step-rules [{:keys [rules]}]
+  (for [[match expansion] rules
+        :let [result1 (apply str (interpose expansion match))
+              result2 (:template (apply-rules {:template result1 :rules rules}))
+              result3 (:template (apply-rules {:template result2 :rules rules}))
+              result4 (:template (apply-rules {:template result3 :rules rules}))]]
+    [match result1 result2 result3 result4 (frequencies result3) (frequencies result4)]))
+
+(comment
+  (step-rules (parse "example"))
+  (step-rules (parse "input")))
+
+;; (time (second (part1 32 (parse "example"))))
 ;; (assert (= 2188189693529 (second (part1 40 (parse "example")))))
 
