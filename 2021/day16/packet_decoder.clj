ecb5a307d64822819fdde3a309d33228362b4379
Charles Comstock <dgtized@gmail.com>
2021-12-16 01:06:52 -0600

    covers cases but still not parsing result

--- 2021/day16/packet_decoder.clj
+++ 2021/day16/packet_decoder.clj
@@ -101,3 +101,15 @@
                  :length 11,
                  :children [{:type :literal, :size 11, :version 6, :type-id 4, :value 15}]}]}]}]
            (decode-packet (bit-string "8A004A801A8002F478"))))
+
+(defn part1 [in]
+  (if (map? in)
+    (apply + (map :version (tree-seq :children :children in)))
+    (apply + (map part1 in))))
+
+(assert (= 16 (part1 (decode-packet (bit-string "8A004A801A8002F478")))))
+(assert (= 12 (part1 (decode-packet (bit-string "620080001611562C8802118E34")))))
+(assert (= 23 (part1 (decode-packet (bit-string "C0015000016115A2E0802F182340")))))
+(assert (= 31 (part1 (decode-packet (bit-string "A0016C880162017C3686B18A3D4780")))))
+
+;; (assert (= 31 (part1 (decode-packet (bit-string (parse "input"))))))
