3a2e52849b727b6995fa02a8852e9eaa5df1c2f2
Charles Comstock <dgtized@gmail.com>
2021-12-17 00:09:30 -0600

    pass around probe and rename it to make-probe

--- 2021/day17/trick_shot.clj
+++ 2021/day17/trick_shot.clj
@@ -10,7 +10,7 @@
 
 ;; (mapv parse [example input])
 
-(defn probe [dx dy]
+(defn make-probe [dx dy]
   {:x 0 :y 0 :dx dx :dy dy})
 
 (defn probe-step [{:keys [x y dx dy] :as probe}]
@@ -28,35 +28,35 @@
 (defn hit? [[[x0 x1] [y0 y1]] {:keys [x y]}]
   (and (<= x0 x x1) (<= y0 y y1)))
 
-(defn trajectory [dx dy target]
+(defn trajectory [probe target]
   (take-while (partial in-bounds? target)
-              (iterate probe-step (probe dx dy))))
+              (iterate probe-step probe)))
 
-(defn summarize [dx dy target]
-  (let [path (trajectory dx dy target)]
+(defn summarize [{:keys [dx dy] :as probe} target]
+  (let [path (trajectory probe target)]
     (when (hit? target (last path))
       [[dx dy] (apply max (map :y path))])))
 
-(assert (summarize 7 2 (parse example)))
-(assert (summarize 6 3 (parse example)))
-(assert (summarize 6 9 (parse example)))
-(assert (summarize 9 0 (parse example)))
-(assert (not (summarize 17 -4 (parse example))))
+(assert (summarize (make-probe 7 2) (parse example)))
+(assert (summarize (make-probe 6 3) (parse example)))
+(assert (summarize (make-probe 6 9) (parse example)))
+(assert (summarize (make-probe 9 0) (parse example)))
+(assert (not (summarize (make-probe 17 -4) (parse example))))
 
 (defn search-max [target]
-  (keep (fn [[dx dy]] (summarize dx dy target))
+  (keep (fn [probe] (summarize probe target))
         (for [dx (range 0 250)
               dy (range 0 300)]
-          [dx dy])))
+          (make-probe dx dy))))
 
 (assert (= [[7 9] 45] (apply max-key second (search-max (parse example)))))
 (assert (= [[17 101] 5151] (apply max-key second (search-max (parse input)))))
 
 (defn search-all [target]
-  (keep (fn [[dx dy]] (summarize dx dy target))
+  (keep (fn [probe] (summarize probe target))
         (for [dx (range 0 175)
               dy (range -125 125)]
-          [dx dy])))
+          (make-probe dx dy))))
 
 (assert (= 112 (count (search-all (parse example)))))
 (assert (= 968 (count (search-all (parse input)))))
