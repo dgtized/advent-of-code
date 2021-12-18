7c8fac1a357df3b7857c21501900bb03f4d5b4f8
Charles Comstock <dgtized@gmail.com>
2021-12-18 02:13:17 -0600

    use zippers to solve explode

--- 2021/day18/snailfish.clj
+++ 2021/day18/snailfish.clj
@@ -1,7 +1,7 @@
 (ns snailfish
   (:require [clojure.edn :as edn]
             [clojure.string :as str]
-            [clojure.zip :as zip]))
+            [clojure.zip :as z]))
 
 (defn parse [file]
   (mapv edn/read-string (str/split-lines (slurp file))))
@@ -24,10 +24,63 @@
 (tree-depth [7,[6,[5,[4,[3,2]]]]])
 (tree-depth [[6,[5,[4,[3,2]]]],1])
 
-(zip/down (zip/vector-zip [[[[[9,8],1],2],3],4]))
+(comment
+  (-> (z/vector-zip [1 2 [3 4] 5])
+      z/next
+      z/up
+      z/up)
+
+  (-> (z/vector-zip [1 [2 [3 [4 [5 0]]]]])
+      z/next
+      z/next
+      left-of
+      (z/replace 0)
+      z/root))
+
+(defn left-of [z]
+  (loop [loc z]
+    (cond (and (not= z loc) (int? (z/node loc)))
+          loc
+          (z/left loc)
+          (recur (z/left loc))
+          :else
+          (recur (z/up loc)))))
+
+(defn right-to-zero [z]
+  (loop [loc z]
+    (if (= 0 (z/node loc))
+      loc
+      (recur (z/next loc)))))
+
+(defn right-of [z]
+  (loop [loc z]
+    (cond (and (not= z loc) (int? (z/node loc)))
+          loc
+          (z/end? loc)
+          nil
+          :else
+          (recur (z/next loc)))))
 
 (defn explode-pair [snail]
-  (tree-seq seq? identity snail))
+  (loop [loc (z/vector-zip snail) depth 0]
+    (cond (z/end? loc)
+          (z/node loc)
+          (and (> depth 3) (vector? (z/node loc)))
+          (let [[a b] (z/node loc)
+                explode (z/replace loc 0)
+                explode' (if-let [left (left-of explode)]
+                           (do (println left)
+                               (-> (z/replace left (+ (z/node left) a))
+                                   right-to-zero))
+                           explode)]
+            (z/root (if-let [right (right-of explode')]
+                      (z/replace right (+ (z/node right) b))
+                      explode')))
+
+          (vector? (z/node loc))
+          (recur (z/down loc) (inc depth))
+          (int? (z/node loc))
+          (recur (z/next loc) depth))))
 
 (assert (= [[[[0,9],2],3],4] (explode-pair [[[[[9,8],1],2],3],4])))
 (assert (= [7,[6,[5,[7,0]]]] (explode-pair [7,[6,[5,[4,[3,2]]]]])))
