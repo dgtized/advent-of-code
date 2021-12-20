4249f7dac0f417be3fe8c06717971a0e71f55a87
Charles Comstock <dgtized@gmail.com>
2021-12-19 23:24:29 -0600

    basic system working, convolve main image

--- 2021/day20/trench_map.clj
+++ 2021/day20/trench_map.clj
@@ -2,13 +2,25 @@
   (:require [clojure.string :as str]))
 
 (defn parse [filename]
-  (let [[algorithm & image] (str/split-lines (slurp filename))]
-    {:algorithm algorithm
+  (let [[enhance & image] (str/split-lines (slurp filename))]
+    {:enhance enhance
      :image (rest image)}))
+
+(def char->int {\. 0 \# 1})
 
 (defn image->grid [{:keys [image]}]
   (into {} (for [[j line] (map-indexed vector image)
                  [i character] (map-indexed vector (seq line))]
-             [[i j] (get {\. 0 \# 1} character)])))
+             [[i j] (char->int character)])))
 
 (assert (= {0 15 1 10} (frequencies (vals (image->grid (parse "example"))))))
+
+(defn pixel-offset [image [i j]]
+  (let [bits (for [dj [-1 0 1]
+                   di [-1 0 1]]
+               (get image (mapv + [i j] [dj di]) 0))]
+    (Integer/parseInt (apply str bits) 2)))
+
+(assert (= 456 (pixel-offset (image->grid (parse "example")) [1 1])))
+(assert (= 1 (char->int (nth (:enhance (parse "example")) 34))))
+
