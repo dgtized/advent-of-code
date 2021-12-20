f9f1216c0b413db4d2200e75a50330496a2a9e8d
Charles Comstock <dgtized@gmail.com>
2021-12-20 01:46:54 -0600

    handle edge outlines from all zeroes by flipping missing quantities every other frame

--- 2021/day20/trench_map.clj
+++ 2021/day20/trench_map.clj
@@ -16,13 +16,13 @@
 
 (assert (= {0 15 1 10} (frequencies (vals (image->grid (parse "example"))))))
 
-(defn pixel-offset [image [i j]]
+(defn pixel-offset [image [i j] fallback]
   (let [bits (for [dj [-1 0 1]
                    di [-1 0 1]]
-               (get image (mapv + [i j] [di dj]) 0))]
+               (get image (mapv + [i j] [di dj]) fallback))]
     (Integer/parseInt (apply str bits) 2)))
 
-(assert (= 294 (pixel-offset (image->grid (parse "example")) [1 1])))
+(assert (= 294 (pixel-offset (image->grid (parse "example")) [1 1] 0)))
 (assert (= 1 (char->int (nth (:enhance (parse "example")) 34))))
 
 (defn bounds [image]
@@ -32,12 +32,12 @@
        (inc (apply max (mapv axis coords)))])))
 
 (defn convolve [enhance image]
-  (let [[[x0 x1] [y0 y1]] (bounds image)]
+  (let [[[x0 x1] [y0 y1]] (bounds image)
+        fallback (if (= \# (nth enhance 0)) (mod x0 2) 0)]
     (into {}
           (for [i (range (- x0 1) (+ x1 1))
                 j (range (- y0 1) (+ y1 1))
-                :let [value (char->int (nth enhance (pixel-offset image [i j])))]
-                :when (= value 1)]
+                :let [value (char->int (nth enhance (pixel-offset image [i j] fallback)))]]
             [[i j] value]))))
 
 (defn part1 [{:keys [enhance] :as input} steps]
@@ -59,8 +59,7 @@
          (print-grid (part1 (parse "input") 3))
          )
 (assert (= 35 ((frequencies (vals (part1 (parse "example") 2))) 1)))
-((frequencies (vals (image->grid (parse "input")))) 1)
-((frequencies (vals (part1 (parse "input") 0))) 1)
-((frequencies (vals (part1 (parse "input") 1))) 1)
-((frequencies (vals (part1 (parse "input") 2))) 1)
+(assert (= 3351 ((frequencies (vals (part1 (parse "example") 50))) 1)))
+
+(assert (= 5884 ((frequencies (vals (part1 (parse "input") 2))) 1)))
 
