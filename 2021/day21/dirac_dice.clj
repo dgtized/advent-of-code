aebe5ebc95994d6b77821fb8a080d70af2a70fca
Charles Comstock <dgtized@gmail.com>
2021-12-20 23:51:30 -0600

    first star day21

--- 2021/day21/dirac_dice.clj
+++ 2021/day21/dirac_dice.clj
@@ -12,8 +12,8 @@
   (let [a (get s 1)
         b (get s 2)]
     {:turn 0
-     :players {0 {:score 0 :p a}
-               1 {:score 0 :p b}}}))
+     :players {0 {:p a :score 0}
+               1 {:p b :score 0}}}))
 
 (init-state (parse "example"))
 
@@ -21,14 +21,16 @@
   (inc (mod (+ (dec a) b) 10)))
 
 (defn die-values [rolls]
-  (+ (inc (mod rolls 100))
-     (inc (mod (inc rolls) 100))
-     (inc (mod (inc (inc rolls)) 100))))
+  [(inc (mod rolls 100))
+   (inc (mod (inc rolls) 100))
+   (inc (mod (inc (inc rolls)) 100))])
+
+(die-values (* 3 34))
 
 (defn turn [{:keys [turn players] :as state}]
   (let [player (mod turn 2)
         {:keys [score p]} (get players player)
-        advance (die-values (* turn 3))
+        advance (apply + (die-values (* turn 3)))
         space (add-track p advance)]
     (-> state
         (update :turn inc)
@@ -39,10 +41,21 @@
 (defn best-score [{:keys [players]}]
   (apply max (mapv :score (vals players))))
 
+(defn part1-score [{:keys [turn players]}]
+  (let [min-score (apply min (mapv :score (vals players)))
+        rolls (* turn 3)]
+    (* min-score rolls)))
+
 (defn part1 [filename]
-  (last (take-while (fn [s] (<= (best-score s) 1000))
-                    (iterate turn (init-state (parse filename))))))
+  (->> filename
+       parse
+       init-state
+       (iterate turn)
+       (drop-while (fn [s] (< (best-score s) 1000)))
+       first
+       part1-score))
 
-(take 10 (iterate turn (init-state (parse "example"))))
+;; (take 12 (iterate turn (init-state (parse "example"))))
 
-(part1 "example")
+(assert (= 739785 (part1 "example")))
+(assert (= 929625 (part1 "input")))
