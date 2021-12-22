56d42114cd454b9e6c0383ece165589b81f05114
Charles Comstock <dgtized@gmail.com>
2021-12-22 00:28:49 -0600

    handle part1 day22

--- 2021/day22/reactor_reboot.clj
+++ 2021/day22/reactor_reboot.clj
@@ -15,23 +15,20 @@
        (<= -50 z 50)))
 
 (defn expand [[[[x0 x1] [y0 y1] [z0 z1]] toggle]]
-  (for [x (range x0 (inc x1))
-        y (range y0 (inc y1))
-        z (range z0 (inc z1))
-        :when (inside-50? x y z)]
+  (for [x (range (max x0 -50) (min 51 (inc x1)))
+        y (range (max y0 -50) (min 51 (inc y1)))
+        z (range (max z0 -50) (min 51 (inc z1)))]
     [[x y z] toggle]))
 
 (assert (= 27 (count (expand [[[10 12] [10 12] [10 12]] true]))))
 
-(defn count-on [grid]
-  (reduce-kv (fn [acc k v] (if v (inc acc) acc))
-             0 grid))
-
 (defn part1 [input]
-  (count-on (reduce (fn [s cube]
-                      (reduce
-                       (fn [s2 [c v]] (assoc s2 c v)) s (expand cube)))
-                    {} input)))
+  (reduce (fn [s cube]
+            (reduce
+             (fn [s2 [c v]] (if v (assoc s2 c v) (dissoc s2 c))) s (expand cube)))
+          {} input))
 
-(part1 (parse "example1"))
+(assert (= 39 (time (count (part1 (parse "example"))))))
+(assert (= 590784 (time (count (part1 (parse "example1"))))))
+(assert (= 582644 (time (count (part1 (parse "input"))))))
 
