de182f091604a45442b66db4179fb2c8d25dbf66
Charles Comstock <dgtized@gmail.com>
2021-12-22 23:41:06 -0600

    add some basic helpers for amphipod searching

--- 2021/day23/amphipod.clj
+++ 2021/day23/amphipod.clj
@@ -8,5 +8,30 @@
                    :when (not= c \ )]
                [[i j] c]))))
 
-(parse "result")
+(defn v+ [a b]
+  (mapv + a b))
 
+(defn neighbors [board pos]
+  (for [change [[-1 0] [0 -1] [0 1] [1 0]]
+        :let [neighbor (v+ pos change)
+              value (get board neighbor)]
+        :when value]
+    [neighbor value]))
+
+(assert (= [[[6 3] \#] [[7 2] \B] [[7 4] \#] [[8 3] \#]]
+           (neighbors (parse "example") [7 3])))
+
+(defn pieces [board]
+  (filter (fn [[_ v]] (#{\A \B \C \D} v)) board))
+
+(defn movable-pieces [board]
+  (filter (fn [[p _]]
+            (some (fn [[_ val]] (#{\.} val)) (neighbors board p)))
+          (pieces board)))
+
+(assert (= [[[7 2] \B] [[5 2] \C] [[9 2] \D] [[3 2] \B]]
+           (movable-pieces (parse "example"))))
+
+;; (defn legal-moves [board [i j]])
+;; (defn valuation [board])
+;; (defn )
