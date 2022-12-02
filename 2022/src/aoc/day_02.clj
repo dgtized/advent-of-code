544819b5d18e1423376761f52951e06aa25d22ba
Charles Comstock <dgtized@gmail.com>
2022-12-01 23:12:43 -0600

    part1 day02

--- 2022/src/aoc/day_02.clj
+++ 2022/src/aoc/day_02.clj
@@ -1,11 +1,38 @@
 (ns aoc.day-02
   (:require [clojure.string :as str]))
 
-(defn file-lines [filename]
+(defn parse [filename]
   (-> filename slurp str/split-lines))
 
 ;; not invaliding cache?
 
+(defn round [a b]
+  (let [beats {"A" #{"C" "Z"}
+               "B" #{"A" "X"}
+               "C" #{"B" "Y"}
+               "X" #{"C" "Z"}
+               "Y" #{"A" "X"}
+               "Z" #{"B" "Y"}}
+        score {"X" 1
+               "Y" 2
+               "Z" 3}]
+    (let [loses (get beats a)
+          wins (get beats b)]
+      (+ (get score b)
+         (cond (contains? loses b)
+               0
+               (contains? wins a)
+               6
+               :else
+               3)))))
+
+(defn score [input]
+  (->> input
+       (mapv (fn [line] (str/split line #"\s")))
+       (map (partial apply round))
+       (apply +)))
+
 ;; test
-(file-lines "input/day02.example") 
-(file-lines "input/day02.input")
+(score (parse "input/day02.example"))
+(score (parse "input/day02.input"))
+
