071c39affd7e6f3556b4adf713ed4fc20f1f4966
Charles Comstock <dgtized@gmail.com>
2022-12-02 23:14:26 -0600

    day03 first star

--- 2022/src/aoc/day_03.clj
+++ 2022/src/aoc/day_03.clj
@@ -1,8 +1,25 @@
 (ns aoc.day-03
   (:require [aoc.utility :refer [file->lines verify]]
-            [nextjournal.clerk :as clerk]))
+            [nextjournal.clerk :as clerk]
+            [clojure.set :as set]))
+
+
+(defn score [c]
+  (let [c (int c)]
+    (inc (if (<= (int \A) c (int \Z))
+           (+ 26 (- c (int \A)))
+           (- c (int \a))))))
+
+(comment (score \A)
+         (score \a))
+
+(defn split-sack [line]
+  (let [n (count line)
+        [c1 c2] (map set (split-at (/ n 2) line))]
+    (score (first (set/intersection c1 c2)))))
 
 ^::clerk/no-cache
-(file->lines "input/day03.input")
+(reduce + (map split-sack (file->lines "input/day03.example")))
 ^::clerk/no-cache
-(file->lines "input/day03.example")
+(reduce + (map split-sack (file->lines "input/day03.input")))
+
