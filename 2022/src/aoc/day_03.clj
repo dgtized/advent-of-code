bce5218ddd32f09b76c7d3dfc49955ba38c53236
Charles Comstock <dgtized@gmail.com>
2022-12-03 00:00:26 -0600

    use sum helper method

--- 2022/src/aoc/day_03.clj
+++ 2022/src/aoc/day_03.clj
@@ -3,6 +3,7 @@
    [aoc.utility :refer [file->lines verify]]
    [clojure.set :as set]))
 
+(def sum (partial reduce +))
 (defn score [c]
   (let [c (int c)]
     (inc (if (<= (int \A) c (int \Z))
@@ -17,12 +18,12 @@
         [c1 c2] (map set (split-at (/ n 2) line))]
     (score (first (set/intersection c1 c2)))))
 
-(reduce + (map split-sack (file->lines "input/day03.example")))
-(reduce + (map split-sack (file->lines "input/day03.input")))
+(sum (map split-sack (file->lines "input/day03.example")))
+(sum (map split-sack (file->lines "input/day03.input")))
 
 (defn partition-sack [lines]
   (for [group (partition 3 lines)]
     (score (first (apply set/intersection (map set group))))))
 
-(reduce + (partition-sack (file->lines "input/day03.example")))
-(reduce + (partition-sack (file->lines "input/day03.input")))
+(sum (partition-sack (file->lines "input/day03.example")))
+(sum (partition-sack (file->lines "input/day03.input")))
