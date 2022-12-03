7da809e9c9ea4ddb98b86a2f85ff7277dc4676be
Charles Comstock <dgtized@gmail.com>
2022-12-03 00:17:56 -0600

    view results and interstitial with clerk/table

--- 2022/src/aoc/day_03.clj
+++ 2022/src/aoc/day_03.clj
@@ -1,29 +1,37 @@
 (ns aoc.day-03
+  {:nextjournal.clerk/visibility {:result :hide}}
   (:require
    [aoc.utility :refer [file->lines verify]]
-   [clojure.set :as set]))
+   [clojure.set :as set]
+   [nextjournal.clerk :as clerk]))
 
 (def sum (partial reduce +))
+
 (defn score [c]
   (let [c (int c)]
     (inc (if (<= (int \A) c (int \Z))
            (+ 26 (- c (int \A)))
            (- c (int \a))))))
 
-(verify (= 27 (score \A)))
-(verify (= 1 (score \a)))
-
 (defn split-sack [line]
   (let [n (count line)
         [c1 c2] (map set (split-at (/ n 2) line))]
     (score (first (set/intersection c1 c2)))))
 
-(sum (map split-sack (file->lines "input/day03.example")))
-(sum (map split-sack (file->lines "input/day03.input")))
-
 (defn partition-sack [lines]
   (for [group (partition 3 lines)]
     (score (first (apply set/intersection (map set group))))))
 
-(sum (partition-sack (file->lines "input/day03.example")))
-(sum (partition-sack (file->lines "input/day03.input")))
+{:nextjournal.clerk/visibility {:result :show}}
+;; Verify score method
+(verify (= 27 (score \A)))
+(verify (= 1 (score \a)))
+
+(clerk/table
+ {:head ["input" "star" "answer"]
+  :rows
+  (for [[part op] {"part1" (partial map split-sack)
+                   "part2" partition-sack}
+        file ["input/day03.example" "input/day03.input"]]
+    (let [sacks (op (file->lines file))]
+      [file part (sum sacks) sacks]))})
