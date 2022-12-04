96e5688c05c8b998bb0d93a3ef23201a89a83acb
Charles Comstock <dgtized@gmail.com>
2022-12-03 23:11:43 -0600

    add show for day04

--- 2022/dev/user.clj
+++ 2022/dev/user.clj
@@ -7,5 +7,6 @@
   (clerk/show! 'aoc/index)
   (clerk/show! 'aoc/day-01-calorie-counting)
   (clerk/show! 'aoc/day-02)
-  (clerk/show! 'aoc/day-03))
+  (clerk/show! 'aoc/day-03)
+  (clerk/show! 'aoc/day-04))
 
