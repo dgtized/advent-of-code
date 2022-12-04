1fb2cebdd216b1e57af0ba88e586a368309e2cd4
Charles Comstock <dgtized@gmail.com>
2022-12-03 23:07:19 -0600

    star1 day04

--- 2022/src/aoc/day_04.clj
+++ 2022/src/aoc/day_04.clj
@@ -1,13 +1,22 @@
 (ns aoc.day-04
   (:require
    [aoc.utility :refer [file->lines verify]]
-   [nextjournal.clerk :as clerk]))
+   [nextjournal.clerk :as clerk]
+   [clojure.string :as str]))
+
+(defn parse [input]
+  (->> input
+       (map (fn [line]
+              (mapcat (fn [part] (map parse-long (str/split part #"-"))) (str/split line #","))))
+       (filter (fn [[a b c d]]
+                 (or (and (<= a c) (<= d b))
+                     (and (<= c a) (<= b d)))))))
 
 (clerk/table
  {:head ["input" "star" "answer"]
   :rows
-  (for [[star method] {1 identity
+  (for [[star method] {1 parse
                        2 identity}
         file ["input/day04.example" "input/day04.input"]]
     (let [result (method (file->lines file))]
-      [file star result]))})
+      [file star (count result) result]))})
