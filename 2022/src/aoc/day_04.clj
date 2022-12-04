a329cdd534fc5d231c57f1a03b74aefd30b9c720
Charles Comstock <dgtized@gmail.com>
2022-12-03 23:10:45 -0600

    star2 day04

--- 2022/src/aoc/day_04.clj
+++ 2022/src/aoc/day_04.clj
@@ -4,19 +4,27 @@
    [nextjournal.clerk :as clerk]
    [clojure.string :as str]))
 
-(defn parse [input]
+(defn containing [[a b c d]]
+  (or (and (<= a c) (<= d b))
+      (and (<= c a) (<= b d))))
+
+(defn overlapping [[a b c d]]
+  (or (<= c a d)
+      (<= c b d)
+      (<= a c b)
+      (<= a d b)))
+
+(defn parse [f input]
   (->> input
        (map (fn [line]
               (mapcat (fn [part] (map parse-long (str/split part #"-"))) (str/split line #","))))
-       (filter (fn [[a b c d]]
-                 (or (and (<= a c) (<= d b))
-                     (and (<= c a) (<= b d)))))))
+       (filter f)))
 
 (clerk/table
  {:head ["input" "star" "answer"]
   :rows
-  (for [[star method] {1 parse
-                       2 identity}
+  (for [[star method] {1 (partial parse containing)
+                       2 (partial parse overlapping)}
         file ["input/day04.example" "input/day04.input"]]
     (let [result (method (file->lines file))]
       [file star (count result) result]))})
