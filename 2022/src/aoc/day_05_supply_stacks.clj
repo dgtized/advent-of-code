2195f997a152842da9b3670d0616660345d9780d
Charles Comstock <dgtized@gmail.com>
2022-12-05 12:35:16 -0600

    visualize first 10 moves

--- 2022/src/aoc/day_05_supply_stacks.clj
+++ 2022/src/aoc/day_05_supply_stacks.clj
@@ -40,3 +40,34 @@
  ["input/day05.example" "input/day05.input"]
  (fn [{:keys [result]}]
    [(apply str (map last result))]))
+
+(defn rows->cols [state]
+  (reverse (apply map vector
+                  (let [height (apply max (map count state))]
+                    (for [col state]
+                      (map #(nth col % "") (range height)))))))
+
+#_(rows->cols ['("Z" "N") '("M" "C" "D") '("P")])
+
+(defn state-move? [x]
+  (and (map? x) (contains? x :state) (contains? x :move)))
+
+(clerk/with-viewers
+  (clerk/add-viewers
+   [{:pred vector?
+     :transform-fn (fn [input]
+                     (clerk/col (get input :nextjournal/value)))}
+    {:pred state-move?
+     :transform-fn (fn [input]
+                     (let [{:keys [state move]} (get input :nextjournal/value)
+                           [n src dst] move]
+                       (clerk/row (if (= move nil)
+                                    (clerk/code "initial stack state")
+                                    (clerk/code (str "move " n " from " src " to " dst)))
+                                  (clerk/table (rows->cols state)))))}])
+  (let [[stacks moves] (parse "input/day05.input")
+        state-move (map (fn [s m] {:state s :move m})
+                        (reductions (fn [s m] (apply (partial move reverse) s m))
+                                    stacks moves)
+                        (conj moves nil))]
+    (vec (take 10 state-move))))
