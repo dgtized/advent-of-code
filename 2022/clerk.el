7ec0d60a31d21791e54760084094840095a1df61
Charles Comstock <dgtized@gmail.com>
2022-12-05 10:54:38 -0600

    Add clerk.el helper

---
+++ 2022/clerk.el
@@ -0,0 +1,19 @@
+(require 'cider)
+
+(defun clerk-show ()
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (cider-interactive-eval
+       (concat "(nextjournal.clerk/show! \"" filename "\")")))))
+
+(defun clerk-clear-cache! ()
+  (interactive)
+  (cider-interactive-eval
+   (concat "(nextjournal.clerk/clear-cache!)"))
+  (message "Cache cleared!"))
+
+(define-key cider-mode-map (kbd "<f8>") 'clerk-show)
+(define-key cider-mode-map (kbd "S-<f8>") 'clerk-clear-cache!)
+
+(provide 'clerk)
--- 2022/dev/user.clj
+++ 2022/dev/user.clj
@@ -4,9 +4,5 @@
 (clerk/serve! {:port 7777 :browse? true :watch-paths ["src"]})
 (comment
   (clerk/clear-cache!)
-  (clerk/show! 'aoc/index)
-  (clerk/show! 'aoc/day-01-calorie-counting)
-  (clerk/show! 'aoc/day-02)
-  (clerk/show! 'aoc/day-03)
-  (clerk/show! 'aoc/day-04))
+  (clerk/show! 'aoc/index))
 
