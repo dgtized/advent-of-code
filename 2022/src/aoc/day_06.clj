be1009cceec7ad8b327aac28636a2f90eafd93cb
Charles Comstock <dgtized@gmail.com>
2022-12-05 23:18:43 -0600

    fix the answer table for day06

--- 2022/src/aoc/day_06.clj
+++ 2022/src/aoc/day_06.clj
@@ -13,14 +13,8 @@
                (when (= (count (set group)) size)
                  (+ size i))))))
 
-(process 4 "input/day06.example")
-(process 4 "input/day06.input")
-(process 14 "input/day06.example")
-(process 14 "input/day06.input")
-
 ;; {::clerk/visibility {:result :show}}
-#_(answer-table
-   [(partial process 4) (partial process 14)]
-   ["input/day06.example" "input/day06.input"]
-   (fn [{:keys [result]}]
-     result))
+(answer-table
+ [(partial process 4) (partial process 14)]
+ ["input/day06.example" "input/day06.input"]
+ (fn [{:keys [result]}] [result]))
