c495507304b161d1155a4710a7093f57a497f013
Charles Comstock <dgtized@gmail.com>
2022-12-05 23:16:11 -0600

    add clerk-serve handler

--- 2022/clerk.el
+++ 2022/clerk.el
@@ -9,9 +9,13 @@
 
 (defun clerk-clear-cache! ()
   (interactive)
-  (cider-interactive-eval
-   (concat "(nextjournal.clerk/clear-cache!)"))
+  (cider-interactive-eval "(nextjournal.clerk/clear-cache!)")
   (message "Cache cleared!"))
+
+(defun clerk-serve ()
+  (interactive)
+  (cider-interactive-eval
+   "(nextjournal.clerk/serve! {:port 7777 :browse? true :watch-paths [\"src\"]})"))
 
 (define-key cider-mode-map (kbd "<f8>") 'clerk-show)
 (define-key cider-mode-map (kbd "S-<f8>") 'clerk-clear-cache!)
