68e91e5e131913821b5a98ff261bf67fb51d73cc
Charles Comstock <dgtized@gmail.com>
2022-12-10 00:57:02 -0600

    day10 star2

--- 2022/src/aoc/day_10.clj
+++ 2022/src/aoc/day_10.clj
@@ -49,14 +49,38 @@
          (map (fn [{:keys [cycle reg]}] (* cycle reg)))
          (apply +))))
 
+(defn sprite-hit [{:keys [cycle reg]}]
+  (if (<= (Math/abs (- (mod (dec cycle) 40) reg)) 1)
+    1
+    0))
+
 #_(interpret (parse "input/day10.input"))
+#_(partition 39 (map sprite-hit (interpret (parse "input/day10.example2"))))
 #_(star1 "input/day10.input")
 
 (defn star2 [file]
-  file)
+  (let [states (interpret (parse file))]
+    (into [] (partition 40 (map sprite-hit states)))))
 
 {::clerk/visibility {:result :show}}
 (aoc/answer-table
  [star1 star2]
- (remove #{"input/day10.example2"} (aoc/input-files "day10"))
+ (remove #{"input/day10.example"} (aoc/input-files "day10"))
  (fn [{:keys [result]}] result))
+
+^::clerk/no-cache
+(clerk/with-viewers
+  [{:pred integer?
+    :render-fn '(fn [n] [:div.inline-block
+                        {:style {:width 20 :height 20}
+                         :class (if (pos? n)
+                                  "bg-black"
+                                  "bg-white border-grey border-solid border-2")}])}
+   {:pred (fn [x] (and (vector? x) (every? vector? x)))
+    :render-fn '(fn [rows opts] (into [:div.flex.flex-col] (v/inspect-children opts) rows))}
+   {:pred (fn [x] (and (vector? x) (every? int? x)))
+    :render-fn '(fn [row opts] (into [:div.flex.inline-flex] (v/inspect-children opts) row))}]
+  #_(mapv vec (star2 "input/day10.example2"))
+  (mapv vec (star2 "input/day10.input")))
+
+;; PLEFULPB
