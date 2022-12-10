de750cd31bc0489eb47ea89016649184fed98838
Charles Comstock <dgtized@gmail.com>
2022-12-10 01:51:40 -0600

    replace/inline with grid->table

--- 2022/src/aoc/day_10.clj
+++ 2022/src/aoc/day_10.clj
@@ -68,19 +68,23 @@
  (remove #{"input/day10.example"} (aoc/input-files "day10"))
  (fn [{:keys [result]}] result))
 
-^::clerk/no-cache
-(clerk/with-viewers
-  [{:pred integer?
-    :render-fn '(fn [n] [:div.inline-block
-                        {:style {:width 20 :height 20}
-                         :class (if (pos? n)
-                                  "bg-black"
-                                  "bg-white border-grey border-solid border-2")}])}
-   {:pred (fn [x] (and (vector? x) (every? vector? x)))
-    :render-fn '(fn [rows opts] (into [:div.flex.flex-col] (v/inspect-children opts) rows))}
-   {:pred (fn [x] (and (vector? x) (every? int? x)))
-    :render-fn '(fn [row opts] (into [:div.flex.inline-flex] (v/inspect-children opts) row))}]
-  #_(mapv vec (star2 "input/day10.example2"))
-  (mapv vec (star2 "input/day10.input")))
+(def grid->table
+  {:pred seq
+   :transform-fn clerk/mark-presented
+   :render-fn '(fn [grid]
+                 (into [:table]
+                       (for [row grid]
+                         (into [:tr]
+                               (for [n row]
+                                 [:td
+                                  {:style {:width 20 :height 20}
+                                   :class (if (pos? n)
+                                            "bg-black"
+                                            "bg-white border-grey border-solid border-2")}])))))})
 
+^{::clerk/viewer grid->table}
+(star2 "input/day10.example2")
+
+^{::clerk/viewer grid->table}
+(star2 "input/day10.input")
 ;; PLEFULPB
