f4996d75e7a4cc1e0e9d31bc75b7dd421745cad7
Charles Comstock <dgtized@gmail.com>
2022-12-12 15:52:52 -0600

    Generalize a*-search better

--- 2022/src/aoc/day_12.clj
+++ 2022/src/aoc/day_12.clj
@@ -24,11 +24,6 @@
 
 (find-loc (->grid (parse "input/day12.example")) \S)
 
-(defn backtrack [current visited]
-  (cons current
-        (lazy-seq (when-let [parent (get visited current)]
-                    (backtrack parent visited)))))
-
 (defn v+ [a b] (map + a b))
 
 (defn height [x]
@@ -43,7 +38,13 @@
                    (<= (height value) (inc (height curr))))]
     neighbor))
 
-(defn path [grid source target]
+;; originally adapted from https://github.com/arttuka/astar/blob/master/src/astar/core.cljc
+(defn backtrack [current visited]
+  (cons current
+        (lazy-seq (when-let [parent (get visited current)]
+                    (backtrack parent visited)))))
+
+(defn a*-search [successors cost heuristic source target]
   (loop [visited {}
          queue (dpm/priority-map-keyfn first source [0 0 nil])]
     (when (seq queue)
@@ -53,29 +54,31 @@
           (reverse (backtrack target visited'))
           (recur visited'
                  (reduce (fn [queue node]
-                           (let [score (+ value 1)]
+                           (let [score (+ value (cost current node))]
                              (if (and (not (contains? visited' node))
                                       (or (not (contains? queue node))
                                           (< score (get-in queue [node 1]))))
-                               (assoc queue node [(+ score 1) score current])
+                               (assoc queue node [(+ score (heuristic node)) score current])
                                queue)))
                          (pop queue)
-                         (neighbors grid current))))))))
+                         (successors current))))))))
 
 (defn star1 [file]
   (let [grid (->grid (parse file))
         src (find-loc grid \S)
         dst (find-loc grid \E)
-        grid (assoc grid src \a dst \z)]
-    (count (rest (path grid src dst)))))
+        grid (assoc grid src \a dst \z)
+        path (partial a*-search (partial neighbors grid) (constantly 1) (constantly 1))]
+    (count (rest (path src dst)))))
 
 (defn star2 [file]
   (let [grid (->grid (parse file))
         src (find-loc grid \S)
         dst (find-loc grid \E)
-        grid (assoc grid src \a dst \z)]
+        grid (assoc grid src \a dst \z)
+        path (partial a*-search (partial neighbors grid) (constantly 1) (constantly 1))]
     (apply min (remove #{0}
-                       (map (fn [s] (count (rest (path grid s dst))))
+                       (map (fn [s] (count (rest (path s dst))))
                             (keep (fn [[pos v]] (when (= v \a) pos)) grid))))))
 
 {::clerk/visibility {:result :show}}
