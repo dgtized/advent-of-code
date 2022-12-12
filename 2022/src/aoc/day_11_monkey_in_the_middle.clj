be40aa38f1b28059b3064d14db11699b6d4cf38c
Charles Comstock <dgtized@gmail.com>
2022-12-12 13:58:18 -0600

    replace manual input with parser

--- 2022/src/aoc/day_11_monkey_in_the_middle.clj
+++ 2022/src/aoc/day_11_monkey_in_the_middle.clj
@@ -1,56 +1,46 @@
 (ns aoc.day-11-monkey-in-the-middle
   (:require
    [aoc.utility :as aoc]
-   [nextjournal.clerk :as clerk]))
+   [nextjournal.clerk :as clerk]
+   [clojure.string :as str]))
 
 #_(aoc/day-input :day 11 :name "example" :content "")
 #_(aoc/day-input :day 11)
 
 {::clerk/visibility {:result :hide}}
 
-(def e-mod (* 23 19 13 17))
+(defn split-on-empty-line [file]
+  (str/split (slurp file) #"\n\n"))
 
-(def example
-  [{:items [79 98]
-    :op (fn [x] (* x 19))
-    :test (fn [x] (if (= 0 (mod x 23)) 2 3))}
-   {:items [54 65 75 74]
-    :op (fn [x] (+ x 6))
-    :test (fn [x] (if (= 0 (mod x 19)) 2 0))}
-   {:items [79 60 97]
-    :op (fn [x] (* x x))
-    :test (fn [x] (if (= 0 (mod x 13)) 1 3))}
-   {:items [74]
-    :op (fn [x] (+ x 3))
-    :test (fn [x] (if (= 0 (mod x 17)) 0 1))}])
+(defn parse-one-int [line]
+  (parse-long (first (re-seq #"\d+" line))))
 
-(def i-mod (* 19 7 17 13 11 2 5 3))
+(defn parse-arg [arg]
+  (when-not (= arg "old")
+    (parse-long arg)))
 
-(def input
-  [{:items [72 97]
-    :op (fn [x] (* x 13))
-    :test (fn [x] (if (= 0 (mod x 19)) 5 6))}
-   {:items [55 70 90 74 95]
-    :op (fn [x] (* x x))
-    :test (fn [x] (if (= 0 (mod x 7)) 5 0))}
-   {:items [74 97 66 57]
-    :op (fn [x] (+ x 6))
-    :test (fn [x] (if (= 0 (mod x 17)) 1 0))}
-   {:items [86 54 53]
-    :op (fn [x] (+ x 2))
-    :test (fn [x] (if (= 0 (mod x 13)) 1 2))}
-   {:items [50 65 78 50 62 99]
-    :op (fn [x] (+ x 3))
-    :test (fn [x] (if (= 0 (mod x 11)) 3 7))}
-   {:items [90]
-    :op (fn [x] (+ x 4))
-    :test (fn [x] (if (= 0 (mod x 2)) 4 6))}
-   {:items [88 92 63 94 96 82 53 53]
-    :op (fn [x] (+ x 8))
-    :test (fn [x] (if (= 0 (mod x 5)) 4 7))}
-   {:items [70 60 71 69 77 70 98]
-    :op (fn [x] (* x 7))
-    :test (fn [x] (if (= 0 (mod x 3)) 2 3))}])
+(defn parse-monkey [lines]
+  (let [[_ items operation & condition] (str/split-lines lines)
+        [_ a op b] (re-find #"=\s([^\s]+)\s([\*\+])\s([^\s]+)" operation)
+        [divisor on-true on-false] (mapv parse-one-int condition)]
+    {:items (mapv parse-long (re-seq #"\d+" items))
+     :op (fn [x] ((get {"*" * "+" +} op)
+                 (or (parse-arg a) x)
+                 (or (parse-arg b) x)))
+     :divisor divisor
+     :test (fn [x] (if (= 0 (mod x divisor)) on-true on-false))}))
+
+(defn lcm [input]
+  (apply * (mapv :divisor input)))
+
+(defn parse [file]
+  (mapv parse-monkey (split-on-empty-line file)))
+
+(def example (parse "input/day11.example"))
+(def e-mod (lcm example))
+
+(def input (parse "input/day11.input"))
+(def i-mod (lcm input))
 
 (defn monkey [lcm input i]
   (let [{:keys [items op] :as monkey} (nth input i)]
