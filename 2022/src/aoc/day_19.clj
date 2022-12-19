5891aa6f00e9619967ee0354694b8a1ec994d577
Charles Comstock <dgtized@gmail.com>
2022-12-18 23:58:25 -0600

    simulation is working for day19 part1

--- 2022/src/aoc/day_19.clj
+++ 2022/src/aoc/day_19.clj
@@ -15,11 +15,40 @@
     [(parse-long n)
      (into {} (for [[_ kind costs] (re-seq #"Each (\w+) robot costs ([^\.]+)\." blueprint)]
                 [(keyword kind)
-                 (map (fn [[_ x ingredient]] [(parse-long x) (keyword ingredient)])
-                      (re-seq #"(\d+) (\w+)" costs))]))]))
+                 (into {} (map (fn [[_ x ingredient]] [(keyword ingredient) (parse-long x)])
+                               (re-seq #"(\d+) (\w+)" costs)))]))]))
 
-(parse "input/day19.example")
-(parse "input/day19.input")
+(def example (parse "input/day19.example"))
+(def input (parse "input/day19.input"))
+
+(defn can-purchase? [ingrediants blueprint-cost]
+  (every? (fn [[k v]] (>= (get ingrediants k 0) v))
+          blueprint-cost))
+
+(defn purchase [ingrediants blueprint-cost]
+  (reduce-kv (fn [i k v] (update i k - v)) ingrediants blueprint-cost))
+
+(defn simulate [blueprint path]
+  (reductions
+   (fn [{:keys [bots ingrediants path] :as state} _]
+     (let [bot (first path)
+           blueprint-cost (get blueprint bot)]
+       (-> (if (and bot (can-purchase? ingrediants blueprint-cost))
+             (-> state
+                 (update-in [:bots bot] inc)
+                 (assoc :ingrediants (purchase ingrediants blueprint-cost))
+                 (update :path rest))
+             state)
+           (update :ingrediants
+                   (fn [i]
+                     (reduce-kv (fn [i kind n] (update i kind + n)) i bots))))))
+   {:bots {:ore 1 :clay 0 :obsidian 0 :geode 0}
+    :ingrediants {:ore 0 :clay 0 :obsidian 0 :geode 0}
+    :path path}
+   (range 1 25)))
+
+(simulate (second (first example))
+          [:clay :clay :clay :obsidian :clay :obsidian :geode :geode])
 
 (defn star1 [file]
   file)
