18efcdf9a88b851c236f84cfb301f3274e28a962
Charles Comstock <dgtized@gmail.com>
2022-12-20 23:15:55 -0600

    day21 star1

--- 2022/src/aoc/day_21.clj
+++ 2022/src/aoc/day_21.clj
@@ -1,6 +1,7 @@
 (ns aoc.day-21
   (:require
    [aoc.utility :as aoc]
+   [clojure.string :as str]
    [nextjournal.clerk :as clerk]))
 
 {::clerk/visibility {:result :hide}}
@@ -8,14 +9,35 @@
 #_(aoc/day-input :day 21 :name "example" :content "")
 #_(aoc/day-input :day 21)
 
+(defn parse-eq [s]
+  (cond (re-matches #"\d+" s)
+        (parse-long s)
+        (re-matches #"(\w+) ([\-*+/]) (\w+)" s)
+        (let [[lhs op rhs] (rest (re-matches #"(\w+) ([\-*+/]) (\w+)" s))]
+          {:op (get {"+" + "-" - "/" quot "*" *} op)
+           :deps [lhs rhs]})
+        :else [[s]]))
+
 (defn parse [file]
-  file)
+  (into {}
+        (for [line (aoc/file->lines file)]
+          (let [[monkey eq] (str/split line #": ")]
+            [monkey (parse-eq eq)]))))
 
 (def example (parse "input/day21.example"))
 (def input (parse "input/day21.input"))
 
+(defn evaluate [graph monkey]
+  (let [value (get graph monkey)]
+    (println "e: " monkey value)
+    (if (number? value)
+      value
+      (let [{:keys [op deps]} value]
+        (apply op (map (partial evaluate graph) deps))))))
+
 (defn star1 [file]
-  file)
+  (let [graph (parse file)]
+    (evaluate graph "root")))
 
 (defn star2 [file]
   file)
