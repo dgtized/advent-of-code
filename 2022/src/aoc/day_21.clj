c2b5e3695a11bd5e70ce5d2d008776d7d6dc9994
Charles Comstock <dgtized@gmail.com>
2022-12-21 01:27:42 -0600

    day21 star2

--- 2022/src/aoc/day_21.clj
+++ 2022/src/aoc/day_21.clj
@@ -27,22 +27,89 @@
 (def example (parse "input/day21.example"))
 (def input (parse "input/day21.input"))
 
-(defn evaluate [graph monkey]
+(defn evaluate [show graph monkey]
   (let [value (get graph monkey)]
+    (when show
+      (println monkey ": " value))
     (if (number? value)
       value
       (let [{:keys [op deps]} value]
-        (apply op (map (partial evaluate graph) deps))))))
+        (apply op (map (partial evaluate show graph) deps))))))
 
 (defn star1 [file]
   (let [graph (parse file)]
-    (evaluate graph "root")))
+    (evaluate false graph "root")))
 
-(defn star2 [file]
-  file)
+(defn binary-search [graph rhs expected]
+  (let [zero (evaluate false (assoc graph "humn" 0) rhs)]
+    (loop [guess 1 last-guess 0 last-value zero]
+      (println guess)
+      (let [value (evaluate false (assoc graph "humn" guess) rhs)]
+        (cond (= value expected)
+              guess
+              (> value expected)
+              (recur (* guess 2) guess value)
+              (< value expected)
+              (recur (/ (+ guess last-guess) 2) guess value))))))
+
+(defn simplify [graph]
+  (let [path (reverse (aoc/a*-search (fn [x] (:deps (get graph x)))
+                                     (constantly 1)
+                                     (constantly 1)
+                                     "root" "humn"))
+        graph' (assoc-in graph ["root" :op] =)]
+    (reduce (fn [pathing [child parent]]
+              (let [{:keys [op deps]} (get graph' parent)
+                    [lhs rhs] deps
+                    other (if (= child lhs) rhs lhs)
+                    value (evaluate false graph' other)]
+                (conj pathing
+                      [parent {:op op
+                               :deps (replace {other value} [lhs rhs])}])))
+            [["humn" 0]]
+            (map vector path (rest path)))))
+
+(def inversion {+ - - + quot * * quot = =})
+(def op->str {+ "+" - "-" quot "/" * "*"})
+
+(defn reverse-eval [pathing]
+  (let [pathing (reverse pathing)]
+    (reductions (fn [[value _] [node {:keys [op deps]}]]
+                  (if (= node "root")
+                    [(first (filter number? deps)) "root"]
+                    (let [operator (get inversion op)
+                          [lhs _] deps
+                          deps' (cond (and (number? lhs) (contains? #{quot -} operator))
+                                      [value lhs]
+                                      (and (number? lhs) (= op -))
+                                      [lhs (- value)]
+                                      :else
+                                      (map (fn [v] (if (string? v) value v))
+                                           deps))]
+                      [(apply operator deps')
+                       node
+                       (op->str operator)
+                       deps' (op->str op) deps
+                       (apply operator (map (fn [v] (if (string? v) value v)) deps))])))
+                [0 0]
+                (butlast pathing))))
+
+(reverse-eval (simplify example))
+(reverse-eval (simplify input))
+
+(defn star2 [file human]
+  (let [graph (parse file)
+        root (get graph "root")
+        [rhs lhs] (:deps root)
+        expected (evaluate false graph lhs)
+        value (evaluate false (assoc graph "humn" human) rhs)]
+    [value expected (= value expected)]))
+
+(star2 "input/day21.example" 301)
+(star2 "input/day21.input" 3059361893920)
 
 {::clerk/visibility {:result :show}}
-(aoc/answer-table
- [star1 star2]
- (aoc/input-files "day21")
- (fn [{:keys [result]}] result))
+#_(aoc/answer-table
+   [star1 star2]
+   (aoc/input-files "day21")
+   (fn [{:keys [result]}] result))
