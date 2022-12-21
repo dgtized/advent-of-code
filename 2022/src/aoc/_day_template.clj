d13cf6eae4c3b19d2a0ab8796a1ec5ffbfea70e8
Charles Comstock <dgtized@gmail.com>
2022-12-20 22:58:24 -0600

    add parse and example/input to default template

--- 2022/src/aoc/_day_template.clj
+++ 2022/src/aoc/_day_template.clj
@@ -8,6 +8,12 @@
 #_(aoc/day-input :day 10 :name "example" :content "")
 #_(aoc/day-input :day 10)
 
+(defn parse [file]
+  (slurp file))
+
+(def example (parse "input/day10.example"))
+(def input (parse "input/day10.input"))
+
 (defn star1 [file]
   file)
 
