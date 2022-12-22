a29553ffe256f13ff9897970b997899ff6017e65
Charles Comstock <dgtized@gmail.com>
2022-12-22 00:38:00 -0600

    basic parsing day22

---
+++ 2022/src/aoc/day_22.clj
@@ -0,0 +1,39 @@
+(ns aoc.day-22
+  (:require
+   [aoc.utility :as aoc]
+   [clojure.string :as str]
+   [nextjournal.clerk :as clerk]))
+
+{::clerk/visibility {:result :hide}}
+
+#_(aoc/day-input :day 22 :name "example" :content "")
+#_(aoc/day-input :day 22)
+
+(defn parse-grid [grid]
+  (into {}
+        (for [[j line] (map-indexed vector (str/split-lines grid))
+              [i c] (map-indexed vector (seq line))
+              :when (contains? #{\# \.} c)]
+          [[i j] c])))
+
+(defn parse [file]
+  (let [[grid dirs] (aoc/split-empty-lines (slurp file))]
+    {:grid (parse-grid grid)
+     :path (map vector
+                (re-seq #"[RL]" dirs)
+                (map parse-long (re-seq #"\d+" dirs)))}))
+
+(def example (parse "input/day22.example"))
+(def input (parse "input/day22.input"))
+
+(defn star1 [file]
+  file)
+
+(defn star2 [file]
+  file)
+
+{::clerk/visibility {:result :show}}
+(aoc/answer-table
+ [star1 star2]
+ (aoc/input-files "day22")
+ (fn [{:keys [result]}] result))
