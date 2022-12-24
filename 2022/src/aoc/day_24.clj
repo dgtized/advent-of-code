c11eb8897d9cfdc771269d6e9d3ae2e6b08e466d
Charles Comstock <dgtized@gmail.com>
2022-12-24 00:09:17 -0600

    grid is simulated

--- 2022/src/aoc/day_24.clj
+++ 2022/src/aoc/day_24.clj
@@ -1,6 +1,7 @@
 (ns aoc.day-24
   (:require
    [aoc.utility :as aoc]
+   [clojure.set :as set]
    [nextjournal.clerk :as clerk]))
 
 {::clerk/visibility {:result :hide}}
@@ -12,7 +13,24 @@
 (defn v+ [a b] (mapv + a b))
 
 (def dmap {\> [1 0] \v [0 1] \< [-1 0] \^ [0 -1]})
+(def inverted-dmap (set/map-invert dmap))
 (def cardinal (vals dmap))
+
+(defn make-blizzard [max-x max-y loc dir]
+  (let [[x y] loc]
+    (fn [t]
+      [(case dir
+         [1 0] [(inc (mod (+ (dec x) t) (dec max-x))) y]
+         [0 1] [x (inc (mod (+ (dec y) t) (dec max-y)))]
+         [-1 0] [(inc (mod (- (dec x) t) (dec max-x))) y]
+         [0 -1] [x (inc (mod (- (dec y) t) (dec max-y)))])
+       dir])))
+
+(defn create-blizzards [grid max-x max-y]
+  (keep (fn [[loc c]]
+          (when-let [dir (dmap c)]
+            (make-blizzard max-x max-y loc dir)))
+        grid))
 
 (defn parse [file]
   (let [lines (aoc/file->lines file)
@@ -28,29 +46,37 @@
      :end (some (fn [[i c]] (when (= c \.) [i 0]))
                 (enumerate (last lines)))}))
 
-(defn blizzards [grid]
-  (filter (fn [[loc c]] (when-let [dir (dmap c)]
-                         [loc dir]))
-          grid))
-
-(defn blizzard [max-x max-y loc dir]
-  (fn [t]
-    (let [[x y] loc]
-      (case dir
-        [1 0] [(inc (mod (+ (dec x) t) (dec max-x))) y]
-        [0 1] [x (inc (mod (+ (dec y) t) (dec max-y)))]
-        [-1 0] [(inc (mod (- (dec x) t) (dec max-x))) y]
-        [0 -1] [x (inc (mod (- (dec y) t) (dec max-y)))]))))
-
 (comment
-  (map (blizzard 6 6 [3 3] [1 0])(range 30))
-  (map (blizzard 6 6 [3 3] [-1 0])(range 30))
-  (map (blizzard 6 6 [3 3] [0 1])(range 30))
-  (map (blizzard 6 6 [3 3] [0 -1])(range 30)))
+  (map (make-blizzard 6 6 [3 3] [1 0]) (range 30))
+  (map (make-blizzard 6 6 [3 3] [-1 0]) (range 30))
+  (map (make-blizzard 6 6 [3 3] [0 1]) (range 30))
+  (map (make-blizzard 6 6 [3 3] [0 -1]) (range 30)))
 
 (def example (parse "input/day24.example"))
 (def complex (parse "input/day24.complex"))
 (def input (parse "input/day24.input"))
+
+(defn simulate [{:keys [grid max-x max-y]}]
+  (let [blizzards (create-blizzards grid max-x max-y)
+        grid' (into {} (map (fn [[loc cell]] (if (contains? #{\. \#} cell)
+                                              [loc cell]
+                                              [loc \.]))
+                            grid))]
+    (fn [t]
+      (reduce (fn [g blizzard] (let [[loc dir] (blizzard t)]
+                                (update g loc
+                                        (fn [cell]
+                                          (cond (= cell \.)
+                                                (inverted-dmap dir)
+                                                (char? cell) 2
+                                                :else (inc cell))))))
+              grid' blizzards))))
+
+(defn show-grid [g max-x max-y]
+  (for [y (range (inc max-y))]
+    (apply str (map #(get g [% y]) (range (inc max-x))))))
+
+(show-grid ((simulate example) 4) 6 6)
 
 (defn star1 [file]
   file)
