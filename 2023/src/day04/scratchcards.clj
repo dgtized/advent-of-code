7bf05e34903559d62fc98c36ea5b1e7dd36c8e1f
Charles Comstock <dgtized@gmail.com>
2023-12-03 23:38:12 -0600

    day04 part2

--- 2023/src/day04/scratchcards.clj
+++ 2023/src/day04/scratchcards.clj
@@ -13,17 +13,32 @@
        :winners (mapv parse-long (re-seq #"\d+" winners))
        :scratches (mapv parse-long (re-seq #"\d+" scratches))})))
 
-(defn score [{:keys [winners scratches]}]
-  (let [matches (count (set/intersection (set winners) (set scratches)))]
-    (if (pos-int? matches)
-      (int (Math/pow 2 (dec matches)))
+(defn matches [{:keys [winners scratches]}]
+  (count (set/intersection (set winners) (set scratches))))
+
+(defn score [card]
+  (let [m (matches card)]
+    (if (pos-int? m)
+      (int (Math/pow 2 (dec m)))
       0)))
 
 (defn part1 [input]
   (apply + (map score (parse input))))
 
 (defn part2 [input]
-  )
+  (let [cardset (parse input)]
+    (reduce
+     (fn [cards {:keys [id] :as card}]
+       (let [m (matches card)
+             copies (get cards id 1)]
+         (reduce (fn [cards dup]
+                   (update cards (+ id dup) (fnil + 0) copies))
+                 cards
+                 (range 1 (inc m)))))
+     (zipmap (map :id cardset) (repeat 1)) cardset)))
 
 (assert (= 13 (part1 example)))
 (assert (= 21821 (part1 input)))
+
+(assert (= 30 (apply + (vals (part2 example)))))
+(assert (= 5539496 (apply + (vals (part2 input)))))
