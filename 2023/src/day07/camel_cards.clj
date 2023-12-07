1c47046aedc9e143f60ca5c871b94c2b0a510bc1
Charles Comstock <dgtized@gmail.com>
2023-12-07 03:55:59 -0600

    attempt day07 part2 but not working on test data

--- 2023/src/day07/camel_cards.clj
+++ 2023/src/day07/camel_cards.clj
@@ -58,4 +58,39 @@
   (sort-by first rank-compare in))
 
 (assert (= 6440 (score (part1 (parse example)))))
-(assert (= 250254244 )(score (part1 (parse input))))
+(assert (= 250254244 (score (part1 (parse input)))))
+
+(def joker-cards [\J \2 \3 \4 \5 \6 \7 \8 \9 \T \Q \K \A])
+(def joker-rank (into {} (map vector cards (range))))
+
+(defn jrank [hand]
+  (let [fr (frequencies (seq hand))
+        [best best-n] (apply max-key second fr)]
+    (rank (if (= best \J)
+            (if (= best-n 5)
+              hand
+              (let [[b2 _] (apply max-key second (dissoc fr \J))]
+                (apply str (replace {\J b2} (seq hand)))))
+            (apply str (replace {\J best} (seq hand)))))))
+
+(defn jrank-compare [a b]
+  (let [c (compare (jrank a) (jrank b))]
+    (if (zero? c)
+      (some (fn [cp]
+              (when-not (zero? cp)
+                cp))
+            (map (fn [ac bc]
+                   (compare (get joker-rank ac)
+                            (get joker-rank bc)))
+                 a b))
+      c)))
+
+(defn part2 [in]
+  (sort-by first jrank-compare in))
+
+(map (fn [[h _]] [h (jrank h)]) (parse example))
+(map (fn [[h _]] [h (jrank h)]) (parse input))
+
+(assert (= 5905 (score (part2 (parse example)))))
+;; not 250606082
+(assert (=  (score (part2 (parse input)))))
