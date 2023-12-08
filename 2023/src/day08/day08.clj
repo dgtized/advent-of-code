9d3d9be86a0e6e0cce40d5bd9c7acf608495d62a
Charles Comstock <dgtized@gmail.com>
2023-12-07 22:49:58 -0600

    day08 template

---
+++ 2023/src/day08/day08.clj
@@ -0,0 +1,19 @@
+(ns day08.day08)
+
+(def input (slurp "src/day08/input"))
+(def example (slurp "src/day08/example"))
+
+(defn parse [in]
+  in)
+
+(defn part1 [in]
+  in)
+
+(assert (= (part1 (parse example))))
+(assert (= (part1 (parse input))))
+
+(defn part2 [in]
+  in)
+
+(assert (= (part2 (parse example))))
+(assert (= (part2 (parse input))))
