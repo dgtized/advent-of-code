108bf675bede265005efdd713ea62a6bd4cc529c
Charles Comstock <dgtized@gmail.com>
2023-12-07 22:51:14 -0600

    basic file template per day

---
+++ 2023/template.clj
@@ -0,0 +1,19 @@
+(ns day.template)
+
+(def input (slurp "src/day/input"))
+(def example (slurp "src/day/example"))
+
+(defn parse [in]
+  in)
+
+(defn part1 [in]
+  in)
+
+(assert (= (part1 (parse example))))
+(assert (= (part1 (parse input))))
+
+(defn part2 [in]
+  in)
+
+(assert (= (part2 (parse example))))
+(assert (= (part2 (parse input))))
