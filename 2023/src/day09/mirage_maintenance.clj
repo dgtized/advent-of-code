84e441f82a63c5f1c668447fcfa363b255f66f2d
Charles Comstock <dgtized@gmail.com>
2023-12-08 23:17:04 -0600

    day09 part1

---
+++ 2023/src/day09/mirage_maintenance.clj
@@ -0,0 +1,32 @@
+(ns day09.mirage-maintenance
+  (:require [clojure.string :as str]))
+
+(def input (slurp "src/day09/input"))
+(def example (slurp "src/day09/example"))
+
+(defn parse [in]
+  (->> in
+       str/split-lines
+       (map (fn [s] (map parse-long (re-seq #"-?\d+" s))))))
+
+(defn sequences [history]
+  (->> history
+       (iterate (fn [s]
+                  (for [[a b] (partition 2 1 s)]
+                    (- b a))))
+       (take-while #(not (every? (fn [v] (zero? v)) %)))))
+
+(defn part1 [in]
+  (->> in
+       (map sequences)
+       (map (fn [hs] (reduce (fn [acc v] (+ acc v )) (reverse (map last hs)))))
+       (apply +)))
+
+(assert (= 114 (part1 (parse example))))
+(assert (= 1887980197 (part1 (parse input))))
+
+(defn part2 [in]
+  in)
+
+(assert (= 2 (part2 (parse example))))
+(assert (= (part2 (parse input))))
