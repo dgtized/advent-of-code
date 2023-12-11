101e8ea2a95e4c3c445c0e5672d823a8334d46d4
Charles Comstock <dgtized@gmail.com>
2023-12-10 18:20:43 -0600

    attempt to solve part2 with odd/even checks

--- 2023/src/day10/pipe_maze.clj
+++ 2023/src/day10/pipe_maze.clj
@@ -66,8 +66,54 @@
 (assert (= 8 (part1 (parse example3))))
 (assert (= 6640 (part1 (parse input))))
 
-(defn part2 [in]
-  in)
+(defn grid->line
+  [grid path-grid]
+  (let [w (apply max (map first (keys grid)))
+        h (apply max (map second (keys grid)))]
+    (->> (for [j (range (inc h))
+               i (range (inc w))]
+           (if (get path-grid [i j]) (get grid [i j]) \.))
+         (partition (inc w))
+         (mapv vec))))
 
-(assert (= (part2 (parse example))))
-(assert (= (part2 (parse input))))
+(defn odd-even-row [grid row j]
+  (reduce (fn [[isecs  inside] [p i]]
+            (case p \.
+                  (if (odd? isecs)
+                    [isecs (conj inside [i j])]
+                    [isecs inside])
+                  \-
+                  [isecs inside]
+                  \|
+                  [(inc isecs) inside]
+                  \F
+                  [(inc isecs) inside]
+                  \L
+                  [(inc isecs) inside]
+                  \7
+                  [(inc isecs) inside]
+                  \J
+                  [(inc isecs) inside]
+                  \S
+                  (if (and (#{\F \L \-} (get grid [(dec i) j] "."))
+                           (#{\J \7 \-} (get grid [(inc i) j] ".")))
+                    [isecs inside]
+                    [(inc isecs) inside])))
+          [0 []]
+          (map vector row (range (count row)))))
+
+(defn space [grid]
+  (let [path-grid (flood grid (find-start grid))
+        cgrid (grid->line grid path-grid)]
+    [(mapv #(apply str %) cgrid)
+     (map (partial odd-even-row grid) cgrid (range (count grid)))]))
+
+(defn part2 [[g v]]
+  (count (mapcat last v)))
+
+;; (count (filter #(= \. %) (apply concat (part2 (parse input))))) => 6320 to check
+
+(assert (= 4 (part2 (space (parse (slurp "src/day10/contained1"))))))
+(assert (= 8 (part2 (space (parse (slurp "src/day10/contained2"))))))
+(assert (= 10 (part2 (space (parse (slurp "src/day10/contained3"))))))
+(assert (= (part2 (space (parse input)))))
