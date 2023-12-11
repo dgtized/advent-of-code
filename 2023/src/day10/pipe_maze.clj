57fb677e9d86ffdeca9e6dd9bd64030169900227
Charles Comstock <dgtized@gmail.com>
2023-12-10 19:25:22 -0600

    day10 part2 flood fill shenanigans

--- 2023/src/day10/pipe_maze.clj
+++ 2023/src/day10/pipe_maze.clj
@@ -1,5 +1,6 @@
 (ns day10.pipe-maze
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def input (slurp "src/day10/input"))
 (def example (slurp "src/day10/example"))
@@ -76,6 +77,16 @@
          (partition (inc w))
          (mapv vec))))
 
+(defn grid->strings
+  [grid]
+  (let [w (apply max (map first (keys grid)))
+        h (apply max (map second (keys grid)))]
+    (->> (for [j (range (inc h))
+               i (range (inc w))]
+           (get grid [i j]))
+         (partition (inc w))
+         (mapv #(apply str %)))))
+
 (defn odd-even-row [grid row j]
   (reduce (fn [[isecs  inside] [p i]]
             (case p \.
@@ -95,25 +106,79 @@
                   \J
                   [(inc isecs) inside]
                   \S
-                  (if (and (#{\F \L \-} (get grid [(dec i) j] "."))
-                           (#{\J \7 \-} (get grid [(inc i) j] ".")))
+                  (if (and (#{\F \L \-} (get grid [(dec i) j] \.))
+                           (#{\J \7 \-} (get grid [(inc i) j] \.)))
                     [isecs inside]
                     [(inc isecs) inside])))
           [0 []]
           (map vector row (range (count row)))))
 
+(defn double-x [row]
+  (apply str (for [c (seq row)]
+               (str c (case c
+                        \- \-
+                        \F \-
+                        \L \-
+                        \J \.
+                        \7 \.
+                        \| \.
+                        \. \.
+                        \S \S)))))
+
+(defn double-y [row]
+  [row
+   (apply str (for [c (seq row)]
+                (case c
+                  \- \.
+                  \F \|
+                  \L \.
+                  \J \.
+                  \7 \|
+                  \| \|
+                  \. \.
+                  \S \S)))])
+
+(defn reduce-size [cgrid]
+  (vec (for [[row _] (partition 2 2 cgrid)]
+         (apply str (mapv first (partition 2 2 row))))))
+
+(defn add-border [cgrid]
+  (let [w (+ 2 (count (first cgrid)))]
+    (concat [(apply str (repeat w \.))]
+            (mapv (fn [r] (str "." (apply str r) ".")) cgrid)
+            [(apply str (repeat w \.))])))
+
+(defn flood-outside [grid]
+  (loop [outside #{[0 0]} queue [[0 0]] grid grid]
+    (if (empty? queue)
+      grid
+      (let [pos (peek queue)
+            val (get grid pos)]
+        (if (= val \.)
+          (let [neighbors (set (filter #(= \. (get grid %))
+                                       (map (partial v+ pos)
+                                            [[-1 0] [1 0] [0 -1] [0 1]])))]
+            (recur (set/union outside neighbors)
+                   (into (pop queue) neighbors)
+                   (assoc grid pos "O")))
+          (recur outside
+                 (pop queue)
+                 grid))))))
+
 (defn space [grid]
   (let [path-grid (flood grid (find-start grid))
-        cgrid (grid->line grid path-grid)]
-    [(mapv #(apply str %) cgrid)
-     (map (partial odd-even-row grid) cgrid (range (count grid)))]))
+        cgrid (grid->line grid path-grid)
+        bordered (add-border cgrid)
+        doubled (mapv #(apply str %) (mapcat double-y (map double-x bordered)))
+        flooded (grid->strings (flood-outside (->grid doubled)))]
+    (reduce-size flooded)))
 
-(defn part2 [[g v]]
-  (count (mapcat last v)))
+(defn part2 [flooded]
+  (get (frequencies (apply str flooded)) \.))
 
 ;; (count (filter #(= \. %) (apply concat (part2 (parse input))))) => 6320 to check
 
 (assert (= 4 (part2 (space (parse (slurp "src/day10/contained1"))))))
 (assert (= 8 (part2 (space (parse (slurp "src/day10/contained2"))))))
 (assert (= 10 (part2 (space (parse (slurp "src/day10/contained3"))))))
-(assert (= (part2 (space (parse input)))))
+(assert (= 411 (part2 (space (parse input)))))
