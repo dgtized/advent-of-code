e4ef78d15dae278aae3945d08c1a5c8e78a7813d
Charles Comstock <dgtized@gmail.com>
2023-12-11 23:53:19 -0600

    day12 star1

--- 2023/src/day12/hot_springs.clj
+++ 2023/src/day12/hot_springs.clj
@@ -12,11 +12,17 @@
 
 (defn accept? [s nums]
   (let [groups (re-seq #"#+" s)]
-    (and (= (count groups) (count nums))
-         (every? true? (map (fn [g n] (= (count g) n)) groups nums)))))
+    (= (mapv count groups) nums)))
 
 (comment
   (accept? "#.##.##" [1 2 2]))
+
+(defn accept-partial? [s nums]
+  (let [groups (re-seq #"#+" s)]
+    (= (mapv count (butlast groups)) (take (max 0 (dec (count groups))) nums))))
+
+(comment
+  (accept-partial? "#." [2 1]))
 
 (defn generate-n [[s nums]]
   (get (frequencies (seq s)) \? 0))
@@ -24,9 +30,12 @@
 (defn generate [springs nums]
   (loop [coll [""] springs springs]
     (if-let [s (first springs)]
-      (recur (if (= s \?)
-               (mapcat (fn [before] [(str before ".") (str before "#")]) coll)
-               (map (fn [before] (str before s)) coll))
+      (recur (keep
+              (fn [gen] (when (accept-partial? gen nums)
+                         gen))
+              (if (= s \?)
+                (mapcat (fn [before] [(str before ".") (str before "#")]) coll)
+                (map (fn [before] (str before s)) coll)))
              (rest springs))
       coll)))
 
@@ -34,14 +43,15 @@
   (filter (fn [s] (accept? s [3 2 1])) (generate "?###????????" [3,2,1])))
 
 (defn part1 [in]
-  (for [[springs nums] in]
-    (reduce (fn [s e] (if (accept? e nums)
-                       (inc s)
-                       s))
-            0 (generate springs nums))))
+  (apply +
+         (for [[springs nums] in]
+           (reduce (fn [s e] (if (accept? e nums)
+                              (inc s)
+                              s))
+                   0 (generate springs nums)))))
 
-(assert (= (part1 (parse example))))
-(time (assert (= (part1 (parse input)))))
+(assert (= 21 (part1 (parse example))))
+(time (assert (= 7191 (part1 (parse input)))))
 
 ;; (defn part2 [in]
 ;;   in)
