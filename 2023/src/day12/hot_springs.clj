8f32eb9d3b55a66e80a001c2447022884f7ee71d
Charles Comstock <dgtized@gmail.com>
2023-12-11 23:03:22 -0600

    basic setup for day12

---
+++ 2023/src/day12/hot_springs.clj
@@ -0,0 +1,20 @@
+(ns day12.hot-springs)
+
+(def input (slurp "src/day12/input"))
+(def example-good (slurp "src/day12/example.good"))
+(def example-bad (slurp "src/day12/example.bad"))
+
+(defn parse [in]
+  in)
+
+(defn part1 [in]
+  in)
+
+(assert (= (part1 (parse example-good))))
+(assert (= (part1 (parse input))))
+
+(defn part2 [in]
+  in)
+
+(assert (= (part2 (parse example-good))))
+(assert (= (part2 (parse input))))
