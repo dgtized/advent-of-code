830e170a978e82634081d236f3a7c8a0b8e4e409
Charles Comstock <dgtized@gmail.com>
2023-12-12 01:51:58 -0600

    experiments 1

--- 2023/src/day12/hot_springs.clj
+++ 2023/src/day12/hot_springs.clj
@@ -75,16 +75,21 @@
 (assert (= 8 (accepted-cases (fake-unfold [".??..??...?##." [1,1,3]]))))
 (assert (= 15 (accepted-cases (fake-unfold ["?###????????" [3,2,1]]))))
 
-(defn part2 [in]
-  (apply +'
-         (map (fn [ex]
-                (let [a (accepted-cases ex)
-                      b (accepted-cases (fake-unfold ex))]
-                  (last [a b (bigint (*' a (Math/pow b 4)))])))
-              in)))
+(defn process [in]
+  (map (fn [[syms nums]]
+         (let [a (accepted-cases [syms nums])
+               b (accepted-cases (fake-unfold [syms nums]))
+               c (accepted-cases [(str "?" syms) nums])
+               d (accepted-cases [(str syms "?") nums])
+               e (accepted-cases [(str "?" syms "?") nums])]
+           [a b c d e (bigint (*' a (Math/pow b 4)))]))
+       in))
 
-(assert (= 525152 (part2 (parse example))))
+(defn part2 [in]
+  (apply +' (map last in)))
+
+(assert (= 525152 (part2 (process (parse example)))))
 ;; 2359496412103 is low
 ;; 5065068739921 ;; low
 ;; 7228887346903 ;; high
-;; (assert (= (part2 (parse input))))
+;; (assert (= (part2 (process (parse input)))))
