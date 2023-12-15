375461d6fb0cedebddfc09fb7c89734182639800
Charles Comstock <dgtized@gmail.com>
2023-12-14 23:37:16 -0600

    day15 star2

--- 2023/src/day15/lens_library.clj
+++ 2023/src/day15/lens_library.clj
@@ -21,8 +21,31 @@
 (assert (= 1320 (part1 (parse example))))
 (assert (= 515210 (part1 (parse input))))
 
-(defn part2 [in]
-  in)
+(defn parse-lens [lens]
+  (when-let [m (re-find #"^(\w+)([-=])(\d*)" lens)]
+    (let [[label op val] (rest m)]
+      [label op (parse-long val)])))
 
-(assert (= (part2 (parse example))))
-(assert (= (part2 (parse input))))
+(mapv parse-lens (parse input))
+
+(defn update-box [box lens]
+  (let [[label op val] lens]
+    (vec (if (= op "-")
+           (remove (fn [l] (= (first l) label)) box)
+           (if-let [existing (some (fn [l] (when (= (first l) label) l)) box)]
+             (replace {existing [label val]} box)
+             (conj box [label val]))))))
+
+(defn hash-boxes [in]
+  (reduce (fn [boxes lens]
+            (update boxes (checksum (first lens)) (fnil update-box []) lens))
+          (vec (repeat 256 []))
+          (map parse-lens in)))
+
+(defn part2 [in]
+  (apply + (mapcat (fn [[i box]]
+                     (map-indexed (fn [slot [_ focal]] (apply * [(inc i) (inc slot) focal])) box))
+                   (map-indexed vector (hash-boxes in)))))
+
+(assert (= 145 (part2 (parse example))))
+(assert (= 246762 (part2 (parse input))))
