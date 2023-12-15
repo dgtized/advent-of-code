b6e69129c41882541e16ce82d04d654e0bf8422b
Charles Comstock <dgtized@gmail.com>
2023-12-14 23:00:43 -0600

    day15 setup conditions

---
+++ 2023/src/day15/lens_library.clj
@@ -0,0 +1,19 @@
+(ns day15.lens-library)
+
+(def input (slurp "src/day15/input"))
+(def example (slurp "src/day15/example"))
+
+(defn parse [in]
+  in)
+
+(defn part1 [in]
+  in)
+
+(assert (= (part1 (parse example))))
+(assert (= (part1 (parse input))))
+
+(defn part2 [in]
+  in)
+
+(assert (= (part2 (parse example))))
+(assert (= (part2 (parse input))))
