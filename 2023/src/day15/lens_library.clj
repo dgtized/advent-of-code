7a23575af216a030420bc41f7521ae574943e77c
Charles Comstock <dgtized@gmail.com>
2023-12-14 23:07:43 -0600

    day15 star1

--- 2023/src/day15/lens_library.clj
+++ 2023/src/day15/lens_library.clj
@@ -1,16 +1,25 @@
-(ns day15.lens-library)
+(ns day15.lens-library
+  (:require [clojure.string :as str]))
 
 (def input (slurp "src/day15/input"))
 (def example (slurp "src/day15/example"))
 
 (defn parse [in]
-  in)
+  (str/split (str/trim in) #","))
+
+(defn checksum [in]
+  (reduce (fn [curr v]
+            (rem (* 17 (+ curr (byte v))) 256))
+          0
+          (seq in)))
+
+(assert (= 52 (checksum "HASH")))
 
 (defn part1 [in]
-  in)
+  (apply + (map checksum in)))
 
-(assert (= (part1 (parse example))))
-(assert (= (part1 (parse input))))
+(assert (= 1320 (part1 (parse example))))
+(assert (= 515210 (part1 (parse input))))
 
 (defn part2 [in]
   in)
