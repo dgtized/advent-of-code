3adea4fc4935ded61586dc1f653ad055923c819a
Charles Comstock <dgtized@gmail.com>
2023-12-19 23:38:36 -0600

    compile reprsentation with transitive closure

--- 2023/src/day20/pulse_propagation.clj
+++ 2023/src/day20/pulse_propagation.clj
@@ -9,16 +9,33 @@
   (for [line (str/split-lines in)]
     (let [[module dests] (str/split line #" -> ")
           type (subs module 0 1)
-          targets (str/split dests #",\s*")]
+          out (str/split dests #",\s*")]
       (if (contains? #{"%" "&"} type)
         {:module (subs module 1)
          :type (if (= type "%")  :flip-flop :conjunction)
-         :targets targets}
+         :out out}
         {:module module
-         :targets targets}))))
+         :out out}))))
+
+(defn compile-state [in]
+  (let [mods (conj (distinct (mapcat (fn [{:keys [module out]}] (conj out module))
+                                     in))
+                   "button")]
+    (reduce
+     (fn [s {:keys [module type out]}]
+       (reduce (fn [s t]
+                 (update-in s [t :in] (fnil conj #{}) module))
+               (update s module merge
+                       {:module module
+                        :out out}
+                       (when type {:type type}))
+               out))
+     (into {} (for [mod mods]
+                [mod {}]))
+     (conj in {:module "button" :out ["broadcast"]}))))
 
 (defn part1 [in]
-  in)
+  (compile-state in))
 
 (assert (= (part1 (parse example))))
 (assert (= (part1 (parse example2))))
