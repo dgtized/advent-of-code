be4dde94e5c40764b0e82fc9e3e1814b8bff7f4d
Charles Comstock <dgtized@gmail.com>
2023-12-20 23:20:11 -0600

    day21 part1

--- 2023/src/day21/step_counter.clj
+++ 2023/src/day21/step_counter.clj
@@ -9,21 +9,34 @@
                  [i c] (map-indexed vector line)]
              [[i j] c])))
 
+(defn v+ [a b] (mapv + a b))
+
 (defn parse [in]
   (->grid (str/split-lines in)))
 
 (defn find-start [in]
   (some (fn [[k v]] (when (= v \S) k)) in))
 
-(defn part1 [in]
-  (let [start (find-start in)]
-    start))
+(defn neighbors [grid pos]
+  (for [dir [[-1 0] [1 0] [0 1] [0 -1]]
+        :let [n (v+ pos dir)]
+        :when (= \. (get grid n))]
+    n))
 
-(assert (= (part1 (parse example))))
-(assert (= (part1 (parse input))))
+(defn steps [grid positions]
+  (->> positions
+       (mapcat (fn [p] (neighbors grid p)))
+       distinct))
+
+(defn part1 [n grid]
+  (let [s (find-start grid)]
+    (nth (iterate (partial steps (assoc grid s \.)) [s]) n)))
+
+(assert (= 16 (count (part1 6 (parse example)))))
+(assert (= 3605 (count (part1 64 (parse input)))))
 
 (defn part2 [in]
   in)
 
 (assert (= (part2 (parse example))))
-(assert (= (part2 (parse input))))
+;; (assert (= (part2 (parse input))))
