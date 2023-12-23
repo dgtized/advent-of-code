d01eeb50dd04ac7fc68377e221ea3d66810cc3b7
Charles Comstock <dgtized@gmail.com>
2023-12-23 03:53:27 -0600

    setup for a long walk

---
+++ 2023/src/day23/a_long_walk.clj
@@ -0,0 +1,78 @@
+(ns day23.a-long-walk
+  (:require [clojure.string :as str]
+            [clojure.data.priority-map :as dpm]))
+
+(def input (slurp "src/day23/input"))
+(def example (slurp "src/day23/example"))
+
+(defn ->grid [input]
+  (into {} (for [[j line] (map-indexed vector input)
+                 [i c] (map-indexed vector line)]
+             [[i j] c])))
+
+(defn parse [in]
+  (->grid (str/split-lines in)))
+
+(defn exit [grid]
+  [(dec (apply max (map first (keys grid))))
+   (apply max (map second (keys grid)))])
+
+(defn backtrack [current visited]
+  (cons current
+        (lazy-seq (when-let [parent (get visited current)]
+                    (backtrack parent visited)))))
+
+(defn a*-search
+  [{:keys [successors sources goal?
+           cost heuristic] :or
+    {cost (constantly 1)
+     heuristic (constantly 1)}}]
+  (loop [visited {}
+         queue (reduce (fn [pq s] (assoc pq s [0 0 nil]))
+                       (dpm/priority-map-keyfn first)
+                       sources)]
+    (when (seq queue)
+      (let [[current [_ value prev]] (peek queue)
+            visited' (assoc visited current prev)]
+        (if (goal? current)
+          (reverse (backtrack current visited'))
+          (recur visited'
+                 (reduce (fn [queue node]
+                           (let [score (+ value (cost current node))]
+                             (if (and (not (contains? visited' node))
+                                      (or (not (contains? queue node))
+                                          (< score (get-in queue [node 1]))))
+                               (assoc queue node [(+ score (heuristic node)) score current])
+                               queue)))
+                         (pop queue)
+                         (successors current))))))))
+
+(defn v+ [a b] (mapv + a b))
+
+(defn successors [grid pos]
+  (for [dir (case (get grid pos)
+              \> [[1 0]]
+              \< [[-1 0]]
+              \^ [[0 -1]]
+              \v [[0 1]]
+              [[1 0] [-1 0] [0 -1] [0 1]])
+        :let [pos' (v+ pos dir)]
+        :when (let [v (get grid pos')]
+                (and v (not= v \#)))]
+    pos'))
+
+(comment (successors (parse example) [1 0]))
+
+(defn part1 [grid]
+  (a*-search {:successors (partial successors grid)
+              :sources [[1 0]]
+              :goal? #(= (exit grid) %)}))
+
+(assert (= (count (part1 (parse example)))))
+;; (assert (= (part1 (parse input))))
+
+(defn part2 [in]
+  in)
+
+(assert (= (part2 (parse example))))
+(assert (= (part2 (parse input))))
