73c5e6aa47bdc9f55a7de41936f4a162dea4aaf0
Charles Comstock <dgtized@gmail.com>
2024-12-01 23:09:41 -0600

    day02 part1

---
+++ 2024/src/day02/red_nosed_reports.clj
@@ -0,0 +1,28 @@
+(ns day02.red-nosed-reports
+  (:require [clojure.string :as str]))
+
+(def input (slurp "src/day02/input"))
+(def example (slurp "src/day02/example"))
+
+(defn parse [in]
+  (->> in
+       str/split-lines
+       (map (fn [line] (map parse-long (re-seq #"\d+" line))))))
+
+(defn safe? [report]
+  (let [pairs (partition 2 1 report)]
+    (and (every? (fn [[a b]] (<= 1 (abs (- b a)) 3)) pairs)
+         (or (every? (fn [[a b]] (< a b)) pairs)
+             (every? (fn [[a b]] (> a b)) pairs)))))
+
+(defn part1 [in]
+  (count (filter safe? in)))
+
+(assert (= 2 (part1 (parse example))))
+(assert (= 369 (part1 (parse input))))
+
+(defn part2 [in]
+  in)
+
+(assert (= (part2 (parse example))))
+(assert (= (part2 (parse input))))
