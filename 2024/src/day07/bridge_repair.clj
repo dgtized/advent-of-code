fa438fee13600ba432845ae097fc29a376409acf
Charles Comstock <dgtized@gmail.com>
2024-12-07 01:45:16 -0600

    day07 part2

--- 2024/src/day07/bridge_repair.clj
+++ 2024/src/day07/bridge_repair.clj
@@ -32,8 +32,21 @@
 (assert (= 3749 (part1 (parse example))))
 (assert (= 1985268524462 (part1 (parse input))))
 
-(defn part2 [in]
-  in)
+(defn tree2 [values]
+  (loop [results [(first values)] expr (rest values)]
+    (if-let [v (first expr)]
+      (recur (concat (map (partial * v) results)
+                     (map (partial + v) results)
+                     (map (fn [e] (parse-long (str e v))) results))
+             (rest expr))
+      results)))
 
-(assert (= (part2 (parse example))))
-(assert (= (part2 (parse input))))
+(defn evaluate2 [[expected & values]]
+  (some #{expected} (tree2 values)))
+
+(defn part2 [equations]
+  (reduce + (keep (fn [expr] (evaluate2 expr)) equations)))
+
+(assert (= 11387 (part2 (parse example))))
+;; slow 5565.96ms
+(time (assert (= 150077710195188 (part2 (parse input)))))
