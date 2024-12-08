40fcd1f96a79342cf203e2f0a8a1095eb7a340ca
Charles Comstock <dgtized@gmail.com>
2024-12-07 23:48:18 -0600

    make antinodes fn only calculate positions for a given pair

--- 2024/src/day08/resonant_collinearity.clj
+++ 2024/src/day08/resonant_collinearity.clj
@@ -22,41 +22,37 @@
           {}
           (remove (fn [[_ f]] (= f \.)) grid)))
 
-(defn antinodes [grid cells]
-  (->> cells
-       all-pairs
-       (mapcat (fn [[a b]]
-                 (let [delta (v/v- b a)]
-                   [(v/v- a delta) (v/v+ b delta)])))
-       (filter grid)))
+(defn antinodes-pair [grid]
+  (fn [[a b]]
+    (let [delta (v/v- b a)]
+      (filter grid [(v/v- a delta) (v/v+ b delta)]))))
 
 (comment (frequencies (parse example)))
 
 (defn result [antinodes grid]
   (->> grid
        frequencies
-       (mapcat (fn [[_ cells]] (antinodes grid cells)))
+       (mapcat (fn [[_ cells]]
+                 (mapcat (antinodes grid) (all-pairs cells))))
        distinct
        count))
 
-(assert (= 14 (result antinodes (parse example))))
-(assert (= 269 (result antinodes (parse input))))
+(assert (= 14 (result antinodes-pair (parse example))))
+(assert (= 269 (result antinodes-pair (parse input))))
 
-(defn antinodes-overlap [grid cells]
-  (->> cells
-       all-pairs
-       (mapcat (fn [[a b]]
-                 (let [delta (v/v- b a)]
-                   (concat
-                    (take-while grid (iterate (fn [x] (v/v- x delta)) a))
-                    (take-while grid (iterate (fn [x] (v/v+ x delta)) b))))))))
+(defn antinodes-all [grid]
+  (fn [[a b]]
+    (let [delta (v/v- b a)]
+      (concat
+       (take-while grid (iterate (fn [x] (v/v- x delta)) a))
+       (take-while grid (iterate (fn [x] (v/v+ x delta)) b))))))
 
 (assert
  (= 9 (->> (let [grid (reduce (fn [g [cell v]] (assoc g cell (if (= v \#) \. v))){} (parse (slurp "src/day08/example2")))
                  freqs (get (frequencies grid) \T)]
-             (antinodes-overlap grid freqs))
+             (mapcat (antinodes-all grid) (all-pairs freqs)))
            distinct
            count)))
 
-(assert (= 34 (result antinodes-overlap (parse example))))
-(assert (= 949 (result antinodes-overlap (parse input))))
+(assert (= 34 (result antinodes-all (parse example))))
+(assert (= 949 (result antinodes-all (parse input))))
