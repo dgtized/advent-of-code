c1f7e212df45571fcbfdf9b7f1fb321644a704ff
Charles Comstock <dgtized@gmail.com>
2024-12-13 00:07:33 -0600

    day13 part1

---
+++ 2024/src/day13/claw_contraption.clj
@@ -0,0 +1,51 @@
+(ns day13.claw-contraption
+  (:require [clojure.string :as str]
+            [aoc.vector :as v]
+            [clojure.math :as math]))
+
+(def input (slurp "src/day13/input"))
+(def example (slurp "src/day13/example"))
+
+(defn parse [in]
+  (for [game (str/split in #"\n\n")]
+    (let [[ax ay bx by gx gy] (mapv parse-long (re-seq #"\d+" game))]
+      {:a [ax ay] :b [bx by] :goal [gx gy]})))
+
+(defn calc-max [{[ax ay] :a [bx by] :b [gx gy] :goal :as game}]
+  (assoc game
+         :max
+         {:a [(quot gx ax) (quot gy ay)]
+          :b [(quot gx bx) (quot gy by)]}))
+
+(defn search [{:keys [a b goal max]}]
+  (keep identity
+        (for [push-a (range 0 100)]
+          (let [goal' (v/v- goal (v/v* a push-a))
+                bx (/ (first goal') (first b))
+                by (/ (second goal') (second b))]
+            (when (= bx by)
+              {:a push-a
+               :b bx
+               :cost (+ (* push-a 3) bx)})))))
+
+(defn solve [game]
+  (let [game' (calc-max game)
+        solutions (search game')]
+    (assoc game'
+           :solutions solutions
+           :min-cost (if (seq solutions) (:cost (apply min-key :cost solutions)) 0))))
+
+(defn part1 [in]
+  (map solve in))
+
+(defn score [games]
+  (int (apply + (map :min-cost games))))
+
+(assert (= 480 (score (part1 (parse example)))))
+(assert (= 31589 (score (part1 (parse input)))))
+
+(defn part2 [in]
+  in)
+
+(assert (= (part2 (parse example))))
+(assert (= (part2 (parse input))))
