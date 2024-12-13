d6f35d859b73f2e2edfb35ee413e40fa26c550c3
Charles Comstock <dgtized@gmail.com>
2024-12-13 00:44:02 -0600

    learn why min/max was different on 0 to 100 or 0 to max

--- 2024/src/day13/claw_contraption.clj
+++ 2024/src/day13/claw_contraption.clj
@@ -15,11 +15,12 @@
   (assoc game
          :max
          {:a [(quot gx ax) (quot gy ay)]
+          :ma (apply min [(quot gx ax) (quot gy ay)])
           :b [(quot gx bx) (quot gy by)]}))
 
-(defn search [{:keys [a b goal max]}]
+(defn search [limit {:keys [a b goal max]}]
   (keep identity
-        (for [push-a (range 0 100)]
+        (for [push-a (range 0 (if limit limit (:ma max)))]
           (let [goal' (v/v- goal (v/v* a push-a))
                 bx (/ (first goal') (first b))
                 by (/ (second goal') (second b))]
@@ -30,10 +31,13 @@
 
 (defn solve [game]
   (let [game' (calc-max game)
-        solutions (search game')]
+        solutions (search 100 game')
+        solve-max (search nil game')]
     (assoc game'
            :solutions solutions
-           :min-cost (if (seq solutions) (:cost (apply min-key :cost solutions)) 0))))
+           :solve-max solve-max
+           :min-cost (if (seq solutions) (:cost (apply min-key :cost solutions)) 0)
+           :max-cost (if (seq solve-max) (:cost (apply min-key :cost solve-max)) 0))))
 
 (defn part1 [in]
   (map solve in))
@@ -44,8 +48,14 @@
 (assert (= 480 (score (part1 (parse example)))))
 (assert (= 31589 (score (part1 (parse input)))))
 
+(comment (filter (fn [{:keys [min-cost max-cost] {:keys [ma]} :max}]
+                   (not= min-cost max-cost))
+                 (part1 (parse input))))
+
 (defn part2 [in]
-  in)
+  (->> in
+       (map (fn [game] (update game :goal v/v+ [10000000000000 10000000000000])))
+       (map solve)))
 
 (assert (= (part2 (parse example))))
-(assert (= (part2 (parse input))))
+;; (assert (= (part2 (parse input))))
