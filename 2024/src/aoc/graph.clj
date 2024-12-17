49a7500c454b8da620c325a72757813b44cc62c3
Charles Comstock <dgtized@gmail.com>
2024-12-17 04:55:39 -0600

    use score-succcessors helper function for a*-search

--- 2024/src/aoc/graph.clj
+++ 2024/src/aoc/graph.clj
@@ -11,22 +11,25 @@
            cost heuristic] :or
     {cost (constantly 1)
      heuristic (constantly 1)}}]
-  (loop [visited {}
-         queue (reduce (fn [pq s] (assoc pq s [0 0 nil]))
-                       (dpm/priority-map-keyfn first)
-                       sources)]
-    (when (seq queue)
-      (let [[current [_ value prev]] (peek queue)
-            visited' (assoc visited current prev)]
-        (if (goal? current)
-          (reverse (backtrack current visited'))
-          (recur visited'
-                 (reduce (fn [queue node]
-                           (let [score (+ value (cost current node))]
-                             (if (and (not (contains? visited' node))
-                                      (or (not (contains? queue node))
-                                          (< score (get-in queue [node 1]))))
-                               (assoc queue node [(+ score (heuristic node)) score current])
-                               queue)))
-                         (pop queue)
-                         (successors current))))))))
+  (letfn [(score-successors [current visited value]
+            (fn [queue node]
+              (let [score (+ value (cost current node))]
+                (if (and (not (contains? visited node))
+                         (or (not (contains? queue node))
+                             (< score (get-in queue [node 1]))))
+                  (assoc queue node [(+ score (heuristic node)) score current])
+                  queue))))]
+
+    (loop [visited {}
+           queue (reduce (fn [pq s] (assoc pq s [0 0 nil]))
+                         (dpm/priority-map-keyfn first)
+                         sources)]
+      (when (seq queue)
+        (let [[current [_ value prev]] (peek queue)
+              visited' (assoc visited current prev)]
+          (if (goal? current)
+            (reverse (backtrack current visited'))
+            (recur visited'
+                   (reduce (score-successors current visited' value)
+                           (pop queue)
+                           (successors current)))))))))
