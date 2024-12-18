254615b0a8471c1a90b889275153ef4f950327e0
Charles Comstock <dgtized@gmail.com>
2024-12-17 23:42:58 -0600

    optimize by reversing the search from end to beginning

--- 2024/src/day18/ram_run.clj
+++ 2024/src/day18/ram_run.clj
@@ -38,14 +38,11 @@
 (assert (= 22 (dec (count (part1 (take 12 (parse example)) [6 6])))))
 (assert (= 438 (dec (count (part1 (take 1024 (parse input)) [70 70])))))
 
-(defn part2 [coords bounds start]
-  (loop [i start]
-    (println i)
-    (if (and (< i (count coords))
-             (nil? (time (part1 (take i coords) bounds))))
-      (do (println (nth coords (dec i)))
-          (nth coords (dec i)))
-      (recur (inc i)))))
+(defn part2 [coords bounds]
+  (time (loop [i (count coords)]
+          (if (and (> i 0) (nil? (part1 (take i coords) bounds)))
+            (recur (dec i))
+            (nth coords i)))))
 
-(assert (= [6 1] (part2 (parse example) [6 6] 12)))
-(assert (= [26 22] (part2 (parse input) [70 70] 1024)))
+(assert (= [6 1] (part2 (parse example) [6 6])))
+(assert (= [26 22] (part2 (parse input) [70 70])))
