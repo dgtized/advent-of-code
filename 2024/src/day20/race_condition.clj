9c63f8d2951e697d733595b34ee19e1dbd57b980
Charles Comstock <dgtized@gmail.com>
2024-12-20 00:11:49 -0600

    day20 part2

--- 2024/src/day20/race_condition.clj
+++ 2024/src/day20/race_condition.clj
@@ -1,8 +1,10 @@
 (ns day20.race-condition
-  (:require [clojure.string :as str]
-            [aoc.grid :as ag]
-            [aoc.vector :as v]
-            [aoc.graph :as graph]))
+  (:require
+   [aoc.combinatorics :as ac]
+   [aoc.graph :as graph]
+   [aoc.grid :as ag]
+   [aoc.vector :as v]
+   [clojure.string :as str]))
 
 (def input (slurp "src/day20/input"))
 (def example (slurp "src/day20/example"))
@@ -28,7 +30,6 @@
 (defn cheats [grid limit]
   (let [path (search grid)
         path-idx (into {} (map vector path (range (count path))))]
-    (println path-idx)
     (mapcat (fn [pos]
               (for [dir v/cardinal
                     :let [c1 (v/v+ pos dir)
@@ -42,8 +43,41 @@
 (assert (= (+ 14 14 2 4 2 3 1 1 1 1 1) (count (cheats (parse example) 1))))
 (assert (= 1351 (count (cheats (parse input) 100))))
 
-(defn part2 [in]
-  in)
+(defn cheat-successors [[mx my] grid goal pos]
+  (for [dir v/cardinal
+        :let [p (v/v+ pos dir)
+              [x y] p]
+        :when (and (< 0 x mx)
+                   (< 0 y my)
+                   (or (= \# (get grid p))
+                       (= p goal)))]
+    p))
 
-(assert (= (part2 (parse example))))
-(assert (= (part2 (parse input))))
+(defn cheat-search [region grid pos goal]
+  (graph/a*-search
+   {:successors (partial cheat-successors region grid goal)
+    :sources [pos]
+    :goal? #(= pos goal)}))
+
+(defn manhattan [[x0 y0] [x1 y1]]
+  (+ (abs (- x1 x0))
+     (abs (- y1 y0))))
+
+(defn part2 [grid limit]
+  (let [ks (keys grid)
+        mx (apply max (map first ks))
+        my (apply max (map second ks))
+        path (search grid)
+        path-idx (into {} (map vector path (range (count path))))]
+    (for [[p1 p2] (ac/all-pairs path)
+          :let [cut-cost (manhattan p1 p2)
+                path-cost (- (get path-idx p2) (get path-idx p1))
+                savings (- path-cost cut-cost)]
+          :when (and (<= cut-cost 20)
+                     (< (get path-idx p1) (get path-idx p2))
+                     (>= savings limit))]
+      savings)))
+
+;; (+ 32 31 29 39 25 23 20 19 12 14 12 22 4 3) => 285
+(assert (= (sort-by first (frequencies (part2 (parse example) 50)))))
+(assert (= 966130 (count (part2 (parse input) 100))))
