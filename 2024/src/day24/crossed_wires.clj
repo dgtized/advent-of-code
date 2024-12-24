616871d2dda5ab86f42305f8009516d55123e568
Charles Comstock <dgtized@gmail.com>
2024-12-24 00:53:14 -0600

    some experiments

--- 2024/src/day24/crossed_wires.clj
+++ 2024/src/day24/crossed_wires.clj
@@ -32,10 +32,14 @@
         (recur (remove (fn [[_ _ _ out]] (contains? results out)) gates)
                (reduce (fn [acc [out v]] (assoc acc out v)) wires resolutions))))))
 
+
+(defn wire [prefix digit]
+  (format "%s%02d" prefix digit))
+
 (defn part1 [in]
   (let [wires (run in)]
     (BigInteger. (apply str (reverse (for [d (range 64)
-                                           :let [w (format "z%02d" d)
+                                           :let [w (wire "z" d)
                                                  v (get wires w)]
                                            :when v]
                                        v)))
@@ -45,8 +49,35 @@
 (assert (= 2024 (part1 (parse example2))))
 (assert (= 52038112429798 (part1 (parse input))))
 
+(defn calc [in]
+  (let [wires (run in)]
+    (apply str (reverse (for [d (range 64)
+                              :let [w (wire "z" d)
+                                    v (get wires w)]
+                              :when v]
+                          v)))))
+
 (defn part2 [in]
-  in)
+  (let [orig (run in)]
+    (for [d (range 64)
+          :let [x-wire (wire "x" d)
+                y-wire (wire "y" d)
+                z-wire (wire "z" d)]
+          :when (and (get-in in [:wires x-wire]))
+          :let [place (if (zero? d)
+                        in
+                        (-> in
+                            (assoc-in [:wires x-wire] 0)
+                            (assoc-in [:wires y-wire] 0)))]]
+      [z-wire (for [var [x-wire y-wire]
+                    :let [v (get-in in [:wires var])]
+                    :when v
+                    :let [r (run (assoc-in place [:wires var] 0))
+                          r' (run (assoc-in place [:wires var] 1))]]
+                [var
+                 [(get r (wire "z" (inc d))) (get r z-wire)]
+                 [(get r' (wire "z" (inc d))) (get r' z-wire)]])])))
 
 (assert (= (part2 (parse example))))
+(assert (= (part2 (parse example2))))
 (assert (= (part2 (parse input))))
