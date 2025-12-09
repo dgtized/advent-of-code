56bfa762ab4b3076c927ea592bf037ff0bcbb731
Charles Comstock <dgtized@gmail.com>
2025-12-09 01:20:20 -0600

    attempt to visualize rectangles to find legal ones

--- 2025/src/day08/movie_theater.clj
+++ 2025/src/day08/movie_theater.clj
@@ -39,9 +39,8 @@
 (space-range (parse input))
 (space-range (parse example))
 
-(defn render [points scale size desc]
-  (let [
-        rv (fn [[x y]]
+(defn render [points scale size desc rects]
+  (let [rv (fn [[x y]]
              [(int (* size (/ x scale))) (int (* size (/ y scale)))])
         image (BufferedImage. size size BufferedImage/TYPE_INT_ARGB)
         gr (.getGraphics image)]
@@ -61,10 +60,15 @@
     (let [[x0 y0] (rv (last points))
           [x1 y1] (rv (first points))]
       (doto gr (.drawLine x0 y0 x1 y1)))
+    (doto gr (.setColor Color/green))
+    (doseq [[a b] rects]
+      (let [[x0 y0] (rv a)
+            [x1 y1] (rv b)]
+        (doto gr (.drawRect (min x0 x1) (min y0 y1) (- (max x0 x1) (min x0 x1)) (- (max y0 y1) (min y0 y1))))))
     (ImageIO/write image "png" (File. (str "src/day08/" desc ".png")))))
 
-(render (parse input) 100000.0 1600 "input")
-(render (parse example) 12.0 1600 "example")
+(render (parse input) 100000.0 1600 "input" [])
+(render (parse example) 12.0 1600 "example" [])
 
 (defn distance [[x0 y0] [x1 y1]]
   (math/sqrt (+ (math/pow (- x1 x0) 2) (math/pow (- y1 y0) 2))))
@@ -85,6 +89,34 @@
 (count (filter (fn [[x y]] (and (< x 94876) (< y 48734))) (parse input))) ;; 217
 (count (filter (fn [[x y]] (and (< x 94876) (> y 50058))) (parse input))) ;; 217
 (* 217 2) ;; 434
+
+(defn point-in-rect? [[[x0 y0] [x1 y1]] [x y]]
+  (and (<= (min x0 x1) x (max x0 x1))
+       (<= (min y0 y1) y (max y0 y1))))
+
+(comment (point-in-rect? [[0 0] [10 10]] [0 0])
+         (point-in-rect? [[0 0] [10 10]] [10 0])
+         (point-in-rect? [[0 0] [10 10]] [11 0])
+         (point-in-rect? [[0 0] [10 10]] [0 11])
+         (point-in-rect? [[0 0] [10 10]] [11 11]))
+
+(defn legal-rectangles [p1 dir points]
+  (let [[x1 y1] p1
+        pts (filter (fn [[x y]] (and (< x x1) (dir y y1))) points)]
+    (sort-by second
+             (for [pt pts
+                   :let [rect [pt p1]]
+                   :when (not-any? (fn [p] (point-in-rect? rect p)) (remove (fn [p] (= p pt)) pts))]
+               [rect (area rect)]))))
+
+(legal-rectangles [94876 48734] < (parse input))
+;; 1275903376
+(legal-rectangles [94876 50058] > (parse input))
+;; 1565730054 ;; too low
+
+(render (parse input) 100000 1600 "solutions"
+        [[[4974 34543] [94876 48734]]
+         [[6011 67676] [94876 50058]]])
 
 (defn part2 [in]
   )
